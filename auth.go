61229943537c1d8e3f637e71d5f75223b1e7876d
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-12 21:46:01 -0500

    -   "bufio"

--- auth.go
+++ auth.go
@@ -4,87 +4,12 @@ import (
    "2a.pages.dev/rosso/http"
    "2a.pages.dev/rosso/protobuf"
    "2a.pages.dev/rosso/tls"
-   "bufio"
    "io"
    "net/url"
    "os"
    "strconv"
    "strings"
 )
-
-func (h *Header) Open_Auth(name string) error {
-   file, err := os.Open(name)
-   if err != nil {
-      return err
-   }
-   defer file.Close()
-   h.Auth.Values = read_query(file)
-   return nil
-}
-
-// You can also use host "android.clients.google.com", but it also uses
-// TLS fingerprinting.
-func New_Auth(email, passwd string) (*Response, error) {
-   client := http.Default_Client
-   hello, err := tls.Parse(tls.Android_API)
-   if err != nil {
-      return nil, err
-   }
-   client.Transport = hello.Transport()
-   body := url.Values{
-      "Email": {email},
-      "Passwd": {passwd},
-      "client_sig": {""},
-      // wikipedia.org/wiki/URL_encoding#Types_of_URI_characters
-      "droidguard_results": {"-"},
-   }.Encode()
-   req := http.Post()
-   req.Body_String(body)
-   req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-   req.URL.Host = "android.googleapis.com"
-   req.URL.Path = "/auth"
-   req.URL.Scheme = "https"
-   res, err := client.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   return &Response{res}, nil
-}
-
-func (a *Auth) Exchange() error {
-   // these values take from Android API 28
-   body := url.Values{
-      "Token": {a.Get_Token()},
-      "app": {"com.android.vending"},
-      "client_sig": {"38918a453d07199354f8b19af05ec6562ced5788"},
-      "service": {"oauth2:https://www.googleapis.com/auth/googleplay"},
-   }.Encode()
-   req := http.Post()
-   req.Body_String(body)
-   req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-   req.URL.Host = "android.googleapis.com"
-   req.URL.Path = "/auth"
-   req.URL.Scheme = "https"
-   res, err := http.Default_Client.Do(req)
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   a.Values = read_query(res.Body)
-   return nil
-}
-
-func read_query(read io.Reader) url.Values {
-   values := make(url.Values)
-   scan := bufio.NewScanner(read)
-   for scan.Scan() {
-      key, value, pass := strings.Cut(scan.Text(), "=")
-      if pass {
-         values.Add(key, value)
-      }
-   }
-   return values
-}
 
 type Header struct {
    Auth Auth // Authorization
@@ -163,3 +88,100 @@ func (a Auth) Get_Auth() string {
 func (a Auth) Get_Token() string {
    return a.Get("Token")
 }
+// You can also use host "android.clients.google.com", but it also uses
+// TLS fingerprinting.
+func New_Auth(email, passwd string) (*Response, error) {
+   client := http.Default_Client
+   hello, err := tls.Parse(tls.Android_API)
+   if err != nil {
+      return nil, err
+   }
+   client.Transport = hello.Transport()
+   body := url.Values{
+      "Email": {email},
+      "Passwd": {passwd},
+      "client_sig": {""},
+      // wikipedia.org/wiki/URL_encoding#Types_of_URI_characters
+      "droidguard_results": {"-"},
+   }.Encode()
+   req := http.Post()
+   req.Body_String(body)
+   req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+   req.URL.Host = "android.googleapis.com"
+   req.URL.Path = "/auth"
+   req.URL.Scheme = "https"
+   res, err := client.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   return &Response{res}, nil
+}
+
+// github.com/golang/go/blob/go1.20.4/src/net/url/url.go
+func parse_query(query string) (url.Values, error) {
+   m := make(url.Values)
+   for query != "" {
+      var key string
+      key, query, _ = strings.Cut(query, "\n")
+      key, value, _ := strings.Cut(key, "=")
+      key, err := url.QueryUnescape(key)
+      if err != nil {
+         return nil, err
+      }
+      value, err = url.QueryUnescape(value)
+      if err != nil {
+         return nil, err
+      }
+      m[key] = append(m[key], value)
+   }
+   return m, nil
+}
+
+func (a *Auth) Exchange() error {
+   // these values take from Android API 28
+   body := url.Values{
+      "Token": {a.Get_Token()},
+      "app": {"com.android.vending"},
+      "client_sig": {"38918a453d07199354f8b19af05ec6562ced5788"},
+      "service": {"oauth2:https://www.googleapis.com/auth/googleplay"},
+   }.Encode()
+   req := http.Post()
+   req.Body_String(body)
+   req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+   req.URL.Host = "android.googleapis.com"
+   req.URL.Path = "/auth"
+   req.URL.Scheme = "https"
+   res, err := http.Default_Client.Do(req)
+   if err != nil {
+      return err
+   }
+   defer res.Body.Close()
+   data, err := io.ReadAll(res.Body)
+   if err != nil {
+      return err
+   }
+   a.Values, err = parse_query(string(data))
+   if err != nil {
+      return err
+   }
+   return nil
+}
+
+func (h *Header) Open_Auth(name string) error {
+   data, err := os.ReadFile(name)
+   if err != nil {
+      return err
+   }
+   h.Auth.Values, err = parse_query(string(data))
+   if err != nil {
+      return err
+   }
+   return nil
+}
+
+
+
+
+
+
+
