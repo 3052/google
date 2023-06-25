89e68cde5babc02eb86306c7677e67df6cb2e4ab
Steven Penny <srpen6+4cq2@gmail.com>
2023-06-25 00:02:21 -0500

    +   "sort"

--- details_test.go
+++ details_test.go
@@ -3,33 +3,34 @@ package googleplay
 import (
    "fmt"
    "os"
+   "sort"
    "strconv"
    "testing"
    "time"
 )
 
 var apps = []app_type{
-   {"2023-02-01",1,"com.miui.weather2"},
-   {"2023-02-20",0,"org.videolan.vlc"},
-   {"2023-03-15",0,"com.amctve.amcfullepisodes"},
-   {"2023-03-22",0,"app.source.getcontact"},
-   {"2023-03-23",0,"kr.sira.metal"},
-   {"2023-04-11",0,"com.google.android.apps.walletnfcrel"},
-   {"2023-05-08",0,"br.com.rodrigokolb.realdrum"},
-   {"2023-05-12",2,"com.miHoYo.GenshinImpact"},
-   {"2023-05-15",1,"com.xiaomi.smarthome"},
-   {"2023-05-25",0,"com.clearchannel.iheartradio.controller"},
-   {"2023-05-29",1,"com.supercell.brawlstars"},
-   {"2023-06-02",1,"com.app.xt"},
-   {"2023-06-07",1,"com.sygic.aura"},
-   {"2023-06-13",0,"com.pinterest"},
-   {"2023-06-14",1,"com.madhead.tos.zh"},
-   {"2023-06-15",0,"org.thoughtcrime.securesms"},
-   {"2023-06-15",2,"com.kakaogames.twodin"},
-   {"2023-06-16",0,"com.google.android.youtube"},
-   {"2023-06-18",1,"com.axis.drawingdesk.v3"},
-   {"2023-06-22",0,"com.instagram.android"},
-   {"2023-06-22",1,"com.binance.dev"},
+   {"2023-06-15",2,295924,"com.kakaogames.twodin"},
+   {"2023-06-02",1,524826,"com.app.xt"},
+   {"2023-04-12",0,5083467,"com.amctve.amcfullepisodes"},
+   {"2023-06-14",1,15386744,"com.madhead.tos.zh"},
+   {"2023-06-18",1,17146720,"com.axis.drawingdesk.v3"},
+   {"2023-03-23",0,22080563,"kr.sira.metal"},
+   {"2023-05-15",1,47920365,"com.xiaomi.smarthome"},
+   {"2023-05-12",2,80187885,"com.miHoYo.GenshinImpact"},
+   {"2023-06-22",1,83263469,"com.binance.dev"},
+   {"2023-05-25",0,89650576,"com.clearchannel.iheartradio.controller"},
+   {"2023-06-07",1,89823699,"com.sygic.aura"},
+   {"2023-06-15",0,124259171,"org.thoughtcrime.securesms"},
+   {"2023-05-08",0,142129205,"br.com.rodrigokolb.realdrum"},
+   {"2023-05-10",0,187860132,"app.source.getcontact"},
+   {"2023-05-29",1,326475842,"com.supercell.brawlstars"},
+   {"2023-02-20",0,341682282,"org.videolan.vlc"},
+   {"2023-06-20",0,655061044,"com.google.android.apps.walletnfcrel"},
+   {"2023-06-13",0,895249472,"com.pinterest"},
+   {"2023-02-01",1,1513248093,"com.miui.weather2"},
+   {"2023-06-22",0,4890507365,"com.instagram.android"},
+   {"2023-06-16",0,14207300870,"com.google.android.youtube"},
 }
 
 func Test_Details(t *testing.T) {
@@ -37,12 +38,18 @@ func Test_Details(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
+   home += "/2a/googleplay"
    var head Header
-   head.Read_Auth(home + "/Documents/googleplay.txt")
+   if err := head.Read_Auth(home + "/auth.txt"); err != nil {
+      t.Fatal(err)
+   }
    head.Auth.Exchange()
    for _, app := range apps {
       platform := Platforms[app.platform]
-      head.Read_Device(home + "/Documents/" + platform + ".bin")
+      err := head.Read_Device(home + "/" + platform + ".bin")
+      if err != nil {
+         t.Fatal(err)
+      }
       d, err := head.Details(app.doc)
       if err != nil {
          t.Fatal(err)
@@ -59,24 +66,42 @@ func Test_Details(t *testing.T) {
       if _, err := d.Installation_Size(); err != nil {
          t.Fatal(err)
       }
-      if _, err := d.Num_Downloads(); err != nil {
-         t.Fatal(err)
-      }
       if _, err := d.Currency_Code(); err != nil {
          t.Fatal(err)
       }
-      raw_date, err := d.Upload_Date()
-      if err != nil {
-         t.Fatal(err)
+      {
+         s, err := d.Upload_Date()
+         if err != nil {
+            t.Fatal(err)
+         }
+         d, err := time.Parse("Jan 2, 2006", s)
+         if err != nil {
+            t.Fatal(err)
+         }
+         app.date = d.String()
       }
-      date, err := time.Parse("Jan 2, 2006", raw_date)
-      if err != nil {
-         t.Fatal(err)
+      {
+         d, err := d.Num_Downloads()
+         if err != nil {
+            t.Fatal(err)
+         }
+         app.downloads = d
       }
-      app.date = date.String()
-      fmt.Print(app, ",\n")
       time.Sleep(99 * time.Millisecond)
    }
+   sort.Slice(apps, func(i, j int) bool {
+      return apps[i].downloads < apps[j].downloads
+   })
+   for _, app := range apps {
+      fmt.Print(app, ",\n")
+   }
+}
+
+type app_type struct {
+   date string
+   platform int64 // X-DFE-Device-ID
+   downloads uint64
+   doc string
 }
 
 func (a app_type) String() string {
@@ -86,13 +111,9 @@ func (a app_type) String() string {
    b = append(b, ',')
    b = strconv.AppendInt(b, a.platform, 10)
    b = append(b, ',')
+   b = strconv.AppendUint(b, a.downloads, 10)
+   b = append(b, ',')
    b = strconv.AppendQuote(b, a.doc)
    b = append(b, '}')
    return string(b)
-}
-
-type app_type struct {
-   date string
-   platform int64 // X-DFE-Device-ID
-   doc string
 }
