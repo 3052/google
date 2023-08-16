c17acd30d72713f9d48ebd2167034c75dcc775a0
Steven Penny <srpen6+1268@gmail.com>
2023-08-15 21:28:11 -0500

    new file:   blog/2023-08-15/3.go

---
+++ blog/2023-08-15/3.go
@@ -0,0 +1,71 @@
+package play
+
+import (
+   "encoding/json"
+   "net/http"
+   "net/http/httputil"
+   "net/url"
+   "strings"
+)
+
+type account_lookup struct {
+   host_gaps *http.Cookie
+}
+
+func (e embedded_setup) account_lookup(s *signup) (*account_lookup, error) {
+   body, err := func() (url.Values, error) {
+      f_req, err := json.MarshalIndent([]any{
+         "srpen6",
+         e.user_hash(),
+         []any{},
+         nil,
+         "US",
+         nil,
+         nil,
+         2,
+         1,
+         1,
+         []any{},
+         "srpen6",
+      }, "", " ")
+      if err != nil {
+         return nil, err
+      }
+      println(string(f_req))
+      v := make(url.Values)
+      v.Set("bgRequest", `["identifier",""]`)
+      v.Set("f.req", string(f_req))
+      return v, nil
+   }()
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://accounts.google.com/_/lookup/accountlookup",
+      strings.NewReader(body.Encode()),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.AddCookie(s.host_gaps)
+   req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+   req.Header.Set("Google-Accounts-Xsrf", "1")
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   {
+      b, err := httputil.DumpResponse(res, true)
+      if err != nil {
+         return nil, err
+      }
+      println(string(b))
+   }
+   var lookup account_lookup
+   lookup.host_gaps, err = host_gaps(res)
+   if err != nil {
+      return nil, err
+   }
+   return &lookup, nil
+}
--- blog/2023-08-15/4.go
+++ blog/2023-08-15/4.go
@@ -1,71 +1,16 @@
 package play
 
-import (
-   "encoding/json"
-   "net/http"
-   "net/http/httputil"
-   "net/url"
-   "strings"
-)
+import "net/http"
 
-type account_lookup struct {
-   host_gaps *http.Cookie
-}
-
-func (e embedded_setup) account_lookup(s *signup) (*account_lookup, error) {
-   body, err := func() (url.Values, error) {
-      f_req, err := json.MarshalIndent([]any{
-         "srpen6",
-         e.user_hash(),
-         []any{},
-         nil,
-         "US",
-         nil,
-         nil,
-         2,
-         1,
-         1,
-         []any{},
-         "srpen6",
-      }, "", " ")
-      if err != nil {
-         return nil, err
-      }
-      println(string(f_req))
-      v := make(url.Values)
-      v.Set("bgRequest", `["identifier",""]`)
-      v.Set("f.req", string(f_req))
-      return v, nil
-   }()
-   if err != nil {
-      return nil, err
-   }
+func (a account_lookup) password() (*http.Response, error) {
    req, err := http.NewRequest(
-      "POST", "https://accounts.google.com/_/lookup/accountlookup",
-      strings.NewReader(body.Encode()),
+      "POST",
+      "https://accounts.google.com/signin/v2/challenge/password/empty",
+      nil,
    )
    if err != nil {
       return nil, err
    }
-   req.AddCookie(s.host_gaps)
-   req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-   req.Header.Set("Google-Accounts-Xsrf", "1")
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   {
-      b, err := httputil.DumpResponse(res, true)
-      if err != nil {
-         return nil, err
-      }
-      println(string(b))
-   }
-   var lookup account_lookup
-   lookup.host_gaps, err = host_gaps(res)
-   if err != nil {
-      return nil, err
-   }
-   return &lookup, nil
+   req.AddCookie(a.host_gaps)
+   return http.DefaultClient.Do(req)
 }
