edb2f573cbe02a1fc0071b212e49cedcd5d5255e
Steven Penny <srpen6+1268@gmail.com>
2023-08-29 21:38:16 -0500

    +func acquire_2(h *play.Header, doc string, vc uint64) error {

--- acquire/acquire_test.go
+++ acquire/acquire_test.go
@@ -1,48 +1,44 @@
 package acquire
 
 import (
-	"154.pages.dev/google/play"
-	"154.pages.dev/http/option"
-	"fmt"
-	"os"
-	"testing"
-	"time"
+   "154.pages.dev/google/play"
+   "154.pages.dev/http/option"
+   "fmt"
+   "os"
+   "testing"
+   "time"
 )
 
 const (
-	doc          = "com.google.android.apps.mapslite"
-	version_code = 160
+   doc = "com.google.android.apps.mapslite"
+   version_code = 160
 )
 
 func Test_Acquire(t *testing.T) {
-
-	acquire_2()
-	return
-
-	home, err := os.UserHomeDir()
-	if err != nil {
-		t.Fatal(err)
-	}
-	home += "/google/play"
-	head := new(play.Header)
-	head.Set_Agent(false)
-	option.No_Location()
-	option.Verbose()
-	{
-		b, err := os.ReadFile(home + "/token.txt")
-		if err != nil {
-			t.Fatal(err)
-		}
-		if err := head.Set_Authorization(b); err != nil {
-			t.Fatal(err)
-		}
-	}
-	if err := Acquire(head, doc); err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println("sleep")
-	time.Sleep(9 * time.Second)
-	if err := New_Delivery(head, doc, version_code); err != nil {
-		t.Fatal(err)
-	}
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   home += "/google/play"
+   head := new(play.Header)
+   head.Set_Agent(false)
+   option.No_Location()
+   option.Verbose()
+   {
+      b, err := os.ReadFile(home + "/token.txt")
+      if err != nil {
+         t.Fatal(err)
+      }
+      if err := head.Set_Authorization(b); err != nil {
+         t.Fatal(err)
+      }
+   }
+   if err := acquire_2(head, doc, version_code); err != nil {
+      t.Fatal(err)
+   }
+   fmt.Println("sleep")
+   time.Sleep(9 * time.Second)
+   if err := New_Delivery(head, doc, version_code); err != nil {
+      t.Fatal(err)
+   }
 }
