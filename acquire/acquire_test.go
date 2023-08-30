fdf71f461485ec431ff0dd8c10888f60052a5a7f
Steven Penny <srpen6+1268@gmail.com>
2023-08-29 20:50:49 -0500

    new file:   acquire/acquire_2.go

--- acquire/acquire_test.go
+++ acquire/acquire_test.go
@@ -1,68 +1,48 @@
 package acquire
 
 import (
-   "154.pages.dev/google/acquire"
-   "154.pages.dev/google/play"
-   "154.pages.dev/http/option"
-   "flag"
-   "fmt"
-   "os"
-   "time"
-   "testing"
+	"154.pages.dev/google/play"
+	"154.pages.dev/http/option"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+const (
+	doc          = "com.google.android.apps.mapslite"
+	version_code = 160
 )
 
 func Test_Acquire(t *testing.T) {
-   var head play.Header
-   head.Set_Agent(false)
-   {
-      b, err := os.ReadFile(dir + "/token.txt")
-      if err != nil {
-         return nil, err
-      }
-      if err := head.Set_Authorization(b); err != nil {
-         return nil, err
-      }
-   }
-   {
-      b, err := os.ReadFile(dir + "/" + platform + ".bin")
-      if err != nil {
-         return nil, err
-      }
-      if err := head.Set_Device(b); err != nil {
-         return nil, err
-      }
-   }
-   return &head, nil
-   dir, err := os.UserHomeDir()
-   if err != nil {
-      panic(err)
-   }
-   dir += "/google/play"
-   if err := os.MkdirAll(dir, os.ModePerm); err != nil {
-      panic(err)
-   }
-   option.No_Location()
-   if f.trace {
-      option.Trace()
-   } else {
-      option.Verbose()
-   }
-   platform := play.Platforms[f.platform]
-   switch {
-   case f.device:
-      err := f.do_device_acquire(dir, platform)
-      if err != nil {
-         panic(err)
-      }
-   case f.doc != "":
-      head, err := f.do_header(dir, platform)
-      if err != nil {
-         panic(err)
-      }
-      if err := acquire.Acquire(head, f.doc); err != nil {
-         panic(err)
-      }
-   default:
-      flag.Usage()
-   }
+
+	acquire_2()
+	return
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		t.Fatal(err)
+	}
+	home += "/google/play"
+	head := new(play.Header)
+	head.Set_Agent(false)
+	option.No_Location()
+	option.Verbose()
+	{
+		b, err := os.ReadFile(home + "/token.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := head.Set_Authorization(b); err != nil {
+			t.Fatal(err)
+		}
+	}
+	if err := Acquire(head, doc); err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("sleep")
+	time.Sleep(9 * time.Second)
+	if err := New_Delivery(head, doc, version_code); err != nil {
+		t.Fatal(err)
+	}
 }
--- acquire/delivery.go
+++ acquire/delivery.go
@@ -1,67 +1,61 @@
 package acquire
 
 import (
-   "154.pages.dev/encoding/protobuf"
-   "154.pages.dev/google/play"
-   "errors"
-   "io"
-   "net/http"
-   "net/url"
-   "strconv"
+	"154.pages.dev/encoding/protobuf"
+	"154.pages.dev/google/play"
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"strconv"
 )
 
 const device_ID = "306e9f7f4192be79"
 
-func New_Delivery(h *play.Header, doc string, vc uint64) (*Delivery, error) {
-   req, err := http.NewRequest(
-      "GET", "https://play-fe.googleapis.com/fdfe/delivery", nil,
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.URL.RawQuery = url.Values{
-      "doc": {doc},
-      "vc": {strconv.FormatUint(vc, 10)},
-   }.Encode()
-   req.Header.Set(h.Agent())
-   req.Header.Set(h.Authorization())
-   req.Header.Set("X-DFE-Device-Id", device_ID)
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   data, err := io.ReadAll(res.Body)
-   if err != nil {
-      return nil, err
-   }
-   // ResponseWrapper
-   mes, err := protobuf.Consume(data)
-   if err != nil {
-      return nil, err
-   }
-   // payload
-   mes, _ = mes.Message(1)
-   // deliveryResponse
-   mes, _ = mes.Message(21)
-   status, err := mes.Varint(1)
-   if err != nil {
-      return nil, err
-   }
-   switch status {
-   case 3:
-      return nil, errors.New("purchase required")
-   case 5:
-      return nil, errors.New("invalid version")
-   }
-   mes, err = mes.Message(2)
-   if err != nil {
-      return nil, errors.New("appDeliveryData not found")
-   }
-   return &Delivery{mes}, nil
-}
-
-// AndroidAppDeliveryData
-type Delivery struct {
-   m protobuf.Message
+func New_Delivery(h *play.Header, doc string, vc uint64) error {
+	req, err := http.NewRequest(
+		"GET", "https://play-fe.googleapis.com/fdfe/delivery", nil,
+	)
+	if err != nil {
+		return err
+	}
+	req.URL.RawQuery = url.Values{
+		"doc": {doc},
+		"vc":  {strconv.FormatUint(vc, 10)},
+	}.Encode()
+	req.Header.Set(h.Agent())
+	req.Header.Set(h.Authorization())
+	req.Header.Set("X-DFE-Device-Id", device_ID)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	// ResponseWrapper
+	mes, err := protobuf.Consume(data)
+	if err != nil {
+		return err
+	}
+	// payload
+	mes, _ = mes.Message(1)
+	// deliveryResponse
+	mes, _ = mes.Message(21)
+	status, err := mes.Varint(1)
+	if err != nil {
+		return err
+	}
+	switch status {
+	case 3:
+		return errors.New("purchase required")
+	case 5:
+		return errors.New("invalid version")
+	}
+	if _, err := mes.Message(2); err != nil {
+		return errors.New("appDeliveryData not found")
+	}
+	return nil
 }
