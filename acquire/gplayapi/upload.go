75e26983a38f8eea1595698aeee7eff26e35e9e9
Steven Penny <srpen6+1268@gmail.com>
2023-09-06 22:30:09 -0500

    new file:   acquire/gplayapi/checkin.go

--- acquire/gplayapi/upload.go
+++ acquire/gplayapi/upload.go
@@ -1,155 +1,15 @@
 package gplayapi
 
 import (
+   "154.pages.dev/encoding/protobuf"
    "acquire/gplayapi/gpproto"
    "bytes"
-   "errors"
-   "fmt"
    "google.golang.org/protobuf/proto"
-   "io"
    "net/http"
-   "net/http/httputil"
-   "net/url"
-   "strings"
-
-   "154.pages.dev/encoding/protobuf"
 )
-
-type _AuthData struct {
-   GsfID                          string
-   _AASToken                      string
-   _AuthToken                     string
-   _DeviceCheckInConsistencyToken string
-   _DeviceConfigToken             string
-   _Email                         string
-   _Locale                        string
-}
-
-type _GooglePlayClient struct {
-   AuthData    *_AuthData
-   _DeviceInfo *_DeviceInfo
-}
-
-func doReq(r *http.Request) ([]byte, int, error) {
-   {
-      b, err := httputil.DumpRequest(r, true)
-      if err != nil {
-         return nil, 0, err
-      }
-      fmt.Printf("%q\n\n", b)
-   }
-   res, err := httpClient.Do(r)
-   if err != nil {
-      return nil, 0, err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      return nil, 0, errors.New(res.Status)
-   }
-   b, err := io.ReadAll(res.Body)
-   return b, res.StatusCode, err
-}
-
-func ptrBool(b bool) *bool {
-   return &b
-}
-
-func ptrStr(str string) *string {
-   return &str
-}
-
-func ptrInt32(i int32) *int32 {
-   return &i
-}
-
-func parseResponse(res string) map[string]string {
-   ret := map[string]string{}
-   for _, ln := range strings.Split(res, "\n") {
-      keyVal := strings.SplitN(ln, "=", 2)
-      if len(keyVal) >= 2 {
-         ret[keyVal[0]] = keyVal[1]
-      }
-   }
-   return ret
-}
-
-func (client *_GooglePlayClient) _GenerateGPToken() (string, error) {
-   params := &url.Values{}
-   params.Set("Token", client.AuthData._AASToken)
-   params.Set("app", "com.android.vending")
-   params.Set("client_sig", "38918a453d07199354f8b19af05ec6562ced5788")
-   params.Set("service", "oauth2:https://www.googleapis.com/auth/googleplay")
-   r, _ := http.NewRequest("POST", _UrlAuth+"?"+params.Encode(), nil)
-   b, _, err := doReq(r)
-   if err != nil {
-      return "", nil
-   }
-   resp := parseResponse(string(b))
-   token, ok := resp["Auth"]
-   if !ok {
-      return "", errors.New("authentication failed: could not generate oauth token")
-   }
-   return token, nil
-}
-
-func NewClientWithDeviceInfo(email, aasToken string, deviceInfo *_DeviceInfo) (client *_GooglePlayClient, err error) {
-   authData := &_AuthData{
-      _Email:    email,
-      _AASToken: aasToken,
-      _Locale:   "en_GB",
-   }
-   client = &_GooglePlayClient{AuthData: authData, _DeviceInfo: deviceInfo}
-   checkInResp, err := client.checkIn()
-   if err != nil {
-      return
-   }
-   client.AuthData.GsfID = fmt.Sprintf("%x", checkInResp.GetAndroidId())
-   client.AuthData._DeviceCheckInConsistencyToken = checkInResp.GetDeviceCheckinConsistencyToken()
-   deviceConfigRes, err := client.uploadDeviceConfig()
-   if err != nil {
-      return
-   }
-   authData._DeviceConfigToken = deviceConfigRes.GetUploadDeviceConfigToken()
-   token, err := client._GenerateGPToken()
-   if err != nil {
-      return
-   }
-   authData._AuthToken = token
-   return
-}
-
-const _UrlBase = "https://android.clients.google.com"
-const _UrlFdfe = _UrlBase + "/fdfe"
-const _UrlAuth = _UrlBase + "/auth"
-const _UrlCheckIn = _UrlBase + "/checkin"
-const _UrlUploadDeviceConfig = _UrlFdfe + "/uploadDeviceConfig"
-
-var (
-   err_GPTokenExpired = errors.New("unauthorized, gp token expired")
-
-   httpClient = &http.Client{}
-)
-
-func (client *_GooglePlayClient) checkIn() (resp *gpproto.AndroidCheckinResponse, err error) {
-   b := checkin_body.Append(nil)
-   r, _ := http.NewRequest("POST", _UrlCheckIn, bytes.NewReader(b))
-   r.Header.Set("User-Agent", client._DeviceInfo._GetAuthUserAgent())
-   r.Header.Set("Content-Type", "application/x-protobuffer")
-   r.Header.Set("Host", "android.clients.google.com")
-   b, _, err = doReq(r)
-   if err != nil {
-      return
-   }
-   resp = &gpproto.AndroidCheckinResponse{}
-   err = proto.Unmarshal(b, resp)
-   return
-}
 
 func (client *_GooglePlayClient) uploadDeviceConfig() (*gpproto.UploadDeviceConfigResponse, error) {
-   b, err := proto.Marshal(&gpproto.UploadDeviceConfigRequest{DeviceConfiguration: client._DeviceInfo._GetDeviceConfigProto()})
-   if err != nil {
-      return nil, err
-   }
+   b := upload_body.Append(nil)
    r, _ := http.NewRequest("POST", _UrlUploadDeviceConfig, bytes.NewReader(b))
    data := client.AuthData
    r.Header.Set("Authorization", "Bearer "+data._AuthToken)
@@ -172,32 +32,8 @@ func (client *_GooglePlayClient) uploadDeviceConfig() (*gpproto.UploadDeviceConf
    return resp.Payload.UploadDeviceConfigResponse, nil
 }
 
-var checkin_body = protobuf.Message{
-   protobuf.Field{Number: 4, Type: 2, Value: protobuf.Prefix{
-      protobuf.Field{Number: 1, Type: 2, Value: protobuf.Prefix{
-         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("PQ3B.190705.003")},
-         protobuf.Field{Number: 2, Type: 2, Value: protobuf.Bytes("sargo")},
-         protobuf.Field{Number: 3, Type: 2, Value: protobuf.Bytes("google")},
-         protobuf.Field{Number: 4, Type: 2, Value: protobuf.Bytes("g670-00011-190411-B-5457439")},
-         protobuf.Field{Number: 5, Type: 2, Value: protobuf.Bytes("b4s4-0.1-5613380")},
-         protobuf.Field{Number: 6, Type: 2, Value: protobuf.Bytes("android-google")},
-         protobuf.Field{Number: 7, Type: 0, Value: protobuf.Varint(1694054582)},
-         protobuf.Field{Number: 8, Type: 0, Value: protobuf.Varint(203615028)},
-         protobuf.Field{Number: 9, Type: 2, Value: protobuf.Bytes("sargo")},
-         protobuf.Field{Number: 10, Type: 0, Value: protobuf.Varint(28)},
-         protobuf.Field{Number: 11, Type: 2, Value: protobuf.Bytes("Pixel 3a")},
-         protobuf.Field{Number: 12, Type: 2, Value: protobuf.Bytes("google")},
-         protobuf.Field{Number: 13, Type: 2, Value: protobuf.Bytes("sargo")},
-         protobuf.Field{Number: 14, Type: 0, Value: protobuf.Varint(0)},
-      }},
-      protobuf.Field{Number: 2, Type: 0, Value: protobuf.Varint(0)},
-      protobuf.Field{Number: 6, Type: 2, Value: protobuf.Bytes("334050")},
-      protobuf.Field{Number: 7, Type: 2, Value: protobuf.Bytes("20815")},
-      protobuf.Field{Number: 8, Type: 2, Value: protobuf.Bytes("mobile-notroaming")},
-      protobuf.Field{Number: 9, Type: 0, Value: protobuf.Varint(0)},
-   }},
-   protobuf.Field{Number: 14, Type: 0, Value: protobuf.Varint(3)},
-   protobuf.Field{Number: 18, Type: 2, Value: protobuf.Prefix{
+var upload_body = protobuf.Message{
+   protobuf.Field{Number: 1, Type: 2, Value: protobuf.Prefix{
       protobuf.Field{Number: 1, Type: 0, Value: protobuf.Varint(3)},
       protobuf.Field{Number: 2, Type: 0, Value: protobuf.Varint(1)},
       protobuf.Field{Number: 3, Type: 0, Value: protobuf.Varint(1)},
