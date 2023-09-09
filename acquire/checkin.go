f91e789640e8f456d30208633c037c1c2a16eef3
Steven Penny <srpen6+1268@gmail.com>
2023-09-09 10:52:29 -0500

    -         protobuf.Field{Number: 3, Type: 2, Value: protobuf.Bytes("google")},

--- acquire/checkin.go
+++ acquire/checkin.go
@@ -16,26 +16,8 @@ import (
 var checkin_body = protobuf.Message{
    protobuf.Field{Number: 4, Type: 2, Value: protobuf.Prefix{
       protobuf.Field{Number: 1, Type: 2, Value: protobuf.Prefix{
-         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("PQ3B.190705.003")},
-         protobuf.Field{Number: 2, Type: 2, Value: protobuf.Bytes("sargo")},
-         protobuf.Field{Number: 3, Type: 2, Value: protobuf.Bytes("google")},
-         protobuf.Field{Number: 4, Type: 2, Value: protobuf.Bytes("g670-00011-190411-B-5457439")},
-         protobuf.Field{Number: 5, Type: 2, Value: protobuf.Bytes("b4s4-0.1-5613380")},
-         protobuf.Field{Number: 6, Type: 2, Value: protobuf.Bytes("android-google")},
-         protobuf.Field{Number: 7, Type: 0, Value: protobuf.Varint(1694054582)},
-         protobuf.Field{Number: 8, Type: 0, Value: protobuf.Varint(203615028)},
-         protobuf.Field{Number: 9, Type: 2, Value: protobuf.Bytes("sargo")},
          protobuf.Field{Number: 10, Type: 0, Value: protobuf.Varint(28)},
-         protobuf.Field{Number: 11, Type: 2, Value: protobuf.Bytes("Pixel 3a")},
-         protobuf.Field{Number: 12, Type: 2, Value: protobuf.Bytes("google")},
-         protobuf.Field{Number: 13, Type: 2, Value: protobuf.Bytes("sargo")},
-         protobuf.Field{Number: 14, Type: 0, Value: protobuf.Varint(0)},
       }},
-      protobuf.Field{Number: 2, Type: 0, Value: protobuf.Varint(0)},
-      protobuf.Field{Number: 6, Type: 2, Value: protobuf.Bytes("334050")},
-      protobuf.Field{Number: 7, Type: 2, Value: protobuf.Bytes("20815")},
-      protobuf.Field{Number: 8, Type: 2, Value: protobuf.Bytes("mobile-notroaming")},
-      protobuf.Field{Number: 9, Type: 0, Value: protobuf.Varint(0)},
    }},
    protobuf.Field{Number: 14, Type: 0, Value: protobuf.Varint(3)},
    protobuf.Field{Number: 18, Type: 2, Value: protobuf.Prefix{
