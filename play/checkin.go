c33439fab59c935ff1ca60870a8ed1b0a075dfe7
Steven Penny <srpen6+1268@gmail.com>
2023-09-14 16:41:50 -0500

    +func (c Config) Checkin() ([]byte, error) {

--- play/checkin.go
+++ play/checkin.go
@@ -8,6 +8,72 @@ import (
    "net/http"
 )
 
+func (c Config) Checkin() ([]byte, error) {
+   // Checkin$AndroidCheckinRequest
+   var m protobuf.Message
+   // Logs$AndroidCheckinProto checkin_
+   m.Add(4, func(m *protobuf.Message) {
+      // Logs$AndroidBuildProto build_
+      m.Add(1, func(m *protobuf.Message) {
+         // int sdkVersion_
+         // single APK valid range 14 - 28
+         // multiple APK valid range 14 - math.MaxInt32
+         m.Add_Varint(10, 28)
+      })
+   })
+   // int version
+   // valid range 2 - 3
+   m.Add_Varint(14, 3)
+   // Config$DeviceConfigurationProto deviceConfiguration_
+   m.Add(18, func(m *protobuf.Message) {
+      // int touchScreen
+      m.Add_Varint(1, c.Touch_Screen)
+      // int keyboard
+      m.Add_Varint(2, c.Keyboard)
+      // int navigation
+      m.Add_Varint(3, c.Navigation)
+      // int screenLayout
+      m.Add_Varint(4, c.Screen_Layout)
+      // boolean hasHardKeyboard
+      m.Add_Bool(5, c.Has_Hard_Keyboard)
+      // boolean hasFiveWayNavigation
+      m.Add_Bool(6, c.Has_Five_Way_Navigation)
+      // int screenDensity
+      m.Add_Varint(7, c.Screen_Density)
+      // int glEsVersion
+      m.Add_Varint(8, c.GL_ES_Version)
+      for _, library := range c.System_Shared_Library {
+         // String[] systemSharedLibrary
+         m.Add_String(9, library)
+      }
+      // String[] nativePlatform
+      m.Add_String(11, c.Native_Platform)
+      for _, extension := range c.GL_Extension {
+         // String[] glExtension
+         m.Add_String(15, extension)
+      }
+      // you cannot swap the next two lines:
+      for _, feature := range c.System_Available_Feature {
+         m.Add(26, func(m *protobuf.Message) {
+            // String[] systemAvailableFeature
+            m.Add_String(1, feature)
+         })
+      }
+   })
+   // r.Header.Set("User-Agent", "GoogleAuth/1.4 sargo PQ3B.190705.003")
+   res, err := http.Post(
+      "https://android.googleapis.com/checkin", "application/x-protobuffer",
+      bytes.NewReader(m.Append(nil)),
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      return nil, errors.New(res.Status)
+   }
+   return io.ReadAll(res.Body)
+}
 func (d Device) android_ID() (uint64, error) {
    v, ok := d.m.Fixed64(7) // long androidId_
    if ok {
@@ -95,71 +161,4 @@ var Phone = Config{
    // com.axis.drawingdesk.v3
    // valid range 0x03_00_01 - math.MaxInt32
    GL_ES_Version: 0xFF_FF_FF,
-}
-
-func (c Config) Checkin() ([]byte, error) {
-   // Checkin$AndroidCheckinRequest
-   var m protobuf.Message
-   // Logs$AndroidCheckinProto checkin_
-   m.Add(4, func(m *protobuf.Message) {
-      // Logs$AndroidBuildProto build_
-      m.Add(1, func(m *protobuf.Message) {
-         // int sdkVersion_
-         // single APK valid range 14 - 28
-         // multiple APK valid range 14 - math.MaxInt32
-         m.Add_Varint(10, 28)
-      })
-   })
-   // int version
-   // valid range 2 - 3
-   m.Add_Varint(14, 3)
-   // Config$DeviceConfigurationProto deviceConfiguration_
-   m.Add(18, func(m *protobuf.Message) {
-      // int touchScreen
-      m.Add_Varint(1, c.Touch_Screen)
-      // int keyboard
-      m.Add_Varint(2, c.Keyboard)
-      // int navigation
-      m.Add_Varint(3, c.Navigation)
-      // int screenLayout
-      m.Add_Varint(4, c.Screen_Layout)
-      // boolean hasHardKeyboard
-      m.Add_Bool(5, c.Has_Hard_Keyboard)
-      // boolean hasFiveWayNavigation
-      m.Add_Bool(6, c.Has_Five_Way_Navigation)
-      // int screenDensity
-      m.Add_Varint(7, c.Screen_Density)
-      // int glEsVersion
-      m.Add_Varint(8, c.GL_ES_Version)
-      for _, library := range c.System_Shared_Library {
-         // String[] systemSharedLibrary
-         m.Add_String(9, library)
-      }
-      // String[] nativePlatform
-      m.Add_String(11, c.Native_Platform)
-      for _, extension := range c.GL_Extension {
-         // String[] glExtension
-         m.Add_String(15, extension)
-      }
-      // you cannot swap the next two lines:
-      for _, feature := range c.System_Available_Feature {
-         m.Add(26, func(m *protobuf.Message) {
-            // String[] systemAvailableFeature
-            m.Add_String(1, feature)
-         })
-      }
-   })
-   // r.Header.Set("User-Agent", "GoogleAuth/1.4 sargo PQ3B.190705.003")
-   res, err := http.Post(
-      "https://android.googleapis.com/checkin", "application/x-protobuffer",
-      bytes.NewReader(m.Append(nil)),
-   )
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      return nil, errors.New(res.Status)
-   }
-   return io.ReadAll(res.Body)
 }
