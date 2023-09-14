3f8b90959e1d68a27d977e4ffd54119d5d38b629
Steven Penny <srpen6+1268@gmail.com>
2023-09-13 20:02:40 -0500

    new file:   acquire/go.mod

---
+++ acquire/go.mod
@@ -0,0 +1,3 @@
+module acquire
+
+go 1.21.0
--- play/checkin.go
+++ play/checkin.go
@@ -27,29 +27,111 @@ func Checkin() ([]byte, error) {
    var checkin_body = protobuf.Message{
       protobuf.Field{Number: 4, Type: 2, Value: protobuf.Prefix{
          protobuf.Field{Number: 1, Type: 2, Value: protobuf.Prefix{
-            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("PQ3B.190705.003")},
-            protobuf.Field{Number: 2, Type: 2, Value: protobuf.Bytes("sargo")},
-            protobuf.Field{Number: 3, Type: 2, Value: protobuf.Bytes("google")},
-            protobuf.Field{Number: 4, Type: 2, Value: protobuf.Bytes("g670-00011-190411-B-5457439")},
-            protobuf.Field{Number: 5, Type: 2, Value: protobuf.Bytes("b4s4-0.1-5613380")},
-            protobuf.Field{Number: 6, Type: 2, Value: protobuf.Bytes("android-google")},
-            protobuf.Field{Number: 7, Type: 0, Value: protobuf.Varint(1694054582)},
-            protobuf.Field{Number: 8, Type: 0, Value: protobuf.Varint(203615028)},
-            protobuf.Field{Number: 9, Type: 2, Value: protobuf.Bytes("sargo")},
             protobuf.Field{Number: 10, Type: 0, Value: protobuf.Varint(28)},
-            protobuf.Field{Number: 11, Type: 2, Value: protobuf.Bytes("Pixel 3a")},
-            protobuf.Field{Number: 12, Type: 2, Value: protobuf.Bytes("google")},
-            protobuf.Field{Number: 13, Type: 2, Value: protobuf.Bytes("sargo")},
-            protobuf.Field{Number: 14, Type: 0, Value: protobuf.Varint(0)},
          }},
-         protobuf.Field{Number: 2, Type: 0, Value: protobuf.Varint(0)},
-         protobuf.Field{Number: 6, Type: 2, Value: protobuf.Bytes("334050")},
-         protobuf.Field{Number: 7, Type: 2, Value: protobuf.Bytes("20815")},
-         protobuf.Field{Number: 8, Type: 2, Value: protobuf.Bytes("mobile-notroaming")},
-         protobuf.Field{Number: 9, Type: 0, Value: protobuf.Varint(0)},
-         protobuf.Field{Number: 18, Type: 0, Value: protobuf.Varint(1)},
       }},
       protobuf.Field{Number: 14, Type: 0, Value: protobuf.Varint(3)},
+      protobuf.Field{Number: 18, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 0, Value: protobuf.Varint(3)},
+         protobuf.Field{Number: 2, Type: 0, Value: protobuf.Varint(1)},
+         protobuf.Field{Number: 3, Type: 0, Value: protobuf.Varint(1)},
+         protobuf.Field{Number: 4, Type: 0, Value: protobuf.Varint(2)},
+         protobuf.Field{Number: 5, Type: 0, Value: protobuf.Varint(0)},
+         protobuf.Field{Number: 6, Type: 0, Value: protobuf.Varint(0)},
+         protobuf.Field{Number: 7, Type: 0, Value: protobuf.Varint(490)},
+         protobuf.Field{Number: 8, Type: 0, Value: protobuf.Varint(196610)},
+         // com.amctve.amcfullepisodes
+         protobuf.Field{Number: 9, Type: 2, Value: protobuf.Bytes("org.apache.http.legacy")},
+         // com.binance.dev
+         protobuf.Field{Number: 9, Type: 2, Value: protobuf.Bytes("android.test.runner")},
+         protobuf.Field{Number: 11, Type: 2, Value: protobuf.Bytes("x86")},
+         // com.instagram.android
+         protobuf.Field{Number: 15, Type: 2, Value: protobuf.Bytes("GL_OES_compressed_ETC1_RGB8_texture")},
+         // com.kakaogames.twodin
+         protobuf.Field{Number: 15, Type: 2, Value: protobuf.Bytes("GL_KHR_texture_compression_astc_ldr")},
+         // app.source.getcontact
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.location.gps")},
+         }},
+         // br.com.rodrigokolb.realdrum
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.software.midi")},
+         }},
+         // com.app.xt
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.camera.front")},
+         }},
+         // com.cabify.rider
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.camera.flash")},
+         }},
+         // com.clearchannel.iheartradio.controller
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.microphone")},
+         }},
+         // com.google.android.apps.walletnfcrel
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.software.device_admin")},
+         }},
+         // com.google.android.youtube
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.touchscreen")},
+         }},
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.wifi")},
+         }},
+         // com.madhead.tos.zh
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.sensor.accelerometer")},
+         }},
+         // com.miHoYo.GenshinImpact
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.opengles.aep")},
+         }},
+         // com.pinterest
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.camera")},
+         }},
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.location")},
+         }},
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.screen.portrait")},
+         }},
+         // com.supercell.brawlstars
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.touchscreen.multitouch")},
+         }},
+         // com.sygic.aura
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.location.network")},
+         }},
+         // com.xiaomi.smarthome
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.bluetooth")},
+         }},
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.bluetooth_le")},
+         }},
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.camera.autofocus")},
+         }},
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.usb.host")},
+         }},
+         // kr.sira.metal
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.sensor.compass")},
+         }},
+         // org.thoughtcrime.securesms
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.telephony")},
+         }},
+         // org.videolan.vlc
+         protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+            protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.screen.landscape")},
+         }},
+      }},
    }
    // r.Header.Set("User-Agent", "GoogleAuth/1.4 sargo PQ3B.190705.003")
    res, err := http.Post(
