d4e0f9e085d8bce48ef9f370938ad78937bd86c7
Steven Penny <srpen6+1268@gmail.com>
2023-09-15 17:44:14 -0500

    new file:   acquire/acquire.go

---
+++ acquire/acquire/readme.md
@@ -0,0 +1,5 @@
+# acquire
+
+~~~
+go run . -d com.chess24.application -v 10000957
+~~~
---
+++ acquire/checkin.go
@@ -0,0 +1,255 @@
+package acquire
+
+import (
+   "154.pages.dev/encoding/protobuf"
+   "bytes"
+   "errors"
+   "fmt"
+   "io"
+   "net/http"
+   "net/http/httputil"
+   "net/url"
+   "strconv"
+   "strings"
+)
+
+func (c checkin) android_ID() (string, error) {
+   v, ok := c.m.Fixed64(7)
+   if ok {
+      return strconv.FormatUint(v, 16), nil
+   }
+   return "", errors.New("androidId_")
+}
+
+var checkin_body = protobuf.Message{
+   protobuf.Field{Number: 4, Type: 2, Value: protobuf.Prefix{
+      protobuf.Field{Number: 1, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 10, Type: 0, Value: protobuf.Varint(28)},
+      }},
+   }},
+   protobuf.Field{Number: 14, Type: 0, Value: protobuf.Varint(3)},
+   protobuf.Field{Number: 18, Type: 2, Value: protobuf.Prefix{
+      protobuf.Field{Number: 1, Type: 0, Value: protobuf.Varint(3)},
+      protobuf.Field{Number: 2, Type: 0, Value: protobuf.Varint(1)},
+      protobuf.Field{Number: 3, Type: 0, Value: protobuf.Varint(1)},
+      protobuf.Field{Number: 4, Type: 0, Value: protobuf.Varint(2)},
+      protobuf.Field{Number: 5, Type: 0, Value: protobuf.Varint(0)},
+      protobuf.Field{Number: 6, Type: 0, Value: protobuf.Varint(0)},
+      protobuf.Field{Number: 7, Type: 0, Value: protobuf.Varint(490)},
+      protobuf.Field{Number: 8, Type: 0, Value: protobuf.Varint(196610)},
+      // com.amctve.amcfullepisodes
+      protobuf.Field{Number: 9, Type: 2, Value: protobuf.Bytes("org.apache.http.legacy")},
+      // com.binance.dev
+      protobuf.Field{Number: 9, Type: 2, Value: protobuf.Bytes("android.test.runner")},
+      protobuf.Field{Number: 11, Type: 2, Value: protobuf.Bytes("x86")},
+      // com.instagram.android
+      protobuf.Field{Number: 15, Type: 2, Value: protobuf.Bytes("GL_OES_compressed_ETC1_RGB8_texture")},
+      // com.kakaogames.twodin
+      protobuf.Field{Number: 15, Type: 2, Value: protobuf.Bytes("GL_KHR_texture_compression_astc_ldr")},
+      // app.source.getcontact
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.location.gps")},
+      }},
+      // br.com.rodrigokolb.realdrum
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.software.midi")},
+      }},
+      // com.app.xt
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.camera.front")},
+      }},
+      // com.cabify.rider
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.camera.flash")},
+      }},
+      // com.clearchannel.iheartradio.controller
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.microphone")},
+      }},
+      // com.google.android.apps.walletnfcrel
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.software.device_admin")},
+      }},
+      // com.google.android.youtube
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.touchscreen")},
+      }},
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.wifi")},
+      }},
+      // com.madhead.tos.zh
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.sensor.accelerometer")},
+      }},
+      // com.miHoYo.GenshinImpact
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.opengles.aep")},
+      }},
+      // com.pinterest
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.camera")},
+      }},
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.location")},
+      }},
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.screen.portrait")},
+      }},
+      // com.supercell.brawlstars
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.touchscreen.multitouch")},
+      }},
+      // com.sygic.aura
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.location.network")},
+      }},
+      // com.xiaomi.smarthome
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.bluetooth")},
+      }},
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.bluetooth_le")},
+      }},
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.camera.autofocus")},
+      }},
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.usb.host")},
+      }},
+      // kr.sira.metal
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.sensor.compass")},
+      }},
+      // org.thoughtcrime.securesms
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.telephony")},
+      }},
+      // org.videolan.vlc
+      protobuf.Field{Number: 26, Type: 2, Value: protobuf.Prefix{
+         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Bytes("android.hardware.screen.landscape")},
+      }},
+   }},
+}
+
+func (client *_GooglePlayClient) checkIn() (*checkin, error) {
+   b := checkin_body.Append(nil)
+   r, _ := http.NewRequest("POST", _UrlCheckIn, bytes.NewReader(b))
+   r.Header.Set("User-Agent", "GoogleAuth/1.4 sargo PQ3B.190705.003")
+   r.Header.Set("Content-Type", "application/x-protobuffer")
+   r.Header.Set("Host", "android.clients.google.com")
+   b, _, err := doReq(r)
+   if err != nil {
+      return nil, err
+   }
+   var check checkin
+   check.m, err = protobuf.Consume(b)
+   if err != nil {
+      return nil, err
+   }
+   return &check, nil
+}
+
+func NewClientWithDeviceInfo(email, aasToken string) (*_GooglePlayClient, error) {
+   authData := &_AuthData{
+      _Email:    email,
+      _AASToken: aasToken,
+      _Locale:   "en_GB",
+   }
+   client := _GooglePlayClient{AuthData: authData}
+   checkInResp, err := client.checkIn()
+   if err != nil {
+      return nil, err
+   }
+   client.AuthData.GsfID, err = checkInResp.android_ID()
+   if err != nil {
+      return nil, err
+   }
+   err = client.uploadDeviceConfig()
+   if err != nil {
+      return nil, err
+   }
+   token, err := client._GenerateGPToken()
+   if err != nil {
+      return nil, err
+   }
+   authData._AuthToken = token
+   return &client, nil
+}
+
+type checkin struct {
+   m protobuf.Message
+}
+
+type _AuthData struct {
+   GsfID                          string
+   _AASToken                      string
+   _AuthToken                     string
+   _Email                         string
+   _Locale                        string
+}
+
+type _GooglePlayClient struct {
+   AuthData    *_AuthData
+}
+
+func doReq(r *http.Request) ([]byte, int, error) {
+   {
+      b, err := httputil.DumpRequest(r, true)
+      if err != nil {
+         return nil, 0, err
+      }
+      fmt.Printf("%q\n\n", b)
+   }
+   res, err := httpClient.Do(r)
+   if err != nil {
+      return nil, 0, err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      return nil, 0, errors.New(res.Status)
+   }
+   b, err := io.ReadAll(res.Body)
+   return b, res.StatusCode, err
+}
+
+func parseResponse(res string) map[string]string {
+   ret := map[string]string{}
+   for _, ln := range strings.Split(res, "\n") {
+      keyVal := strings.SplitN(ln, "=", 2)
+      if len(keyVal) >= 2 {
+         ret[keyVal[0]] = keyVal[1]
+      }
+   }
+   return ret
+}
+
+func (client *_GooglePlayClient) _GenerateGPToken() (string, error) {
+   params := &url.Values{}
+   params.Set("Token", client.AuthData._AASToken)
+   params.Set("app", "com.android.vending")
+   params.Set("client_sig", "38918a453d07199354f8b19af05ec6562ced5788")
+   params.Set("service", "oauth2:https://www.googleapis.com/auth/googleplay")
+   r, _ := http.NewRequest("POST", _UrlAuth+"?"+params.Encode(), nil)
+   b, _, err := doReq(r)
+   if err != nil {
+      return "", nil
+   }
+   resp := parseResponse(string(b))
+   token, ok := resp["Auth"]
+   if ok {
+      return token, nil
+   }
+   return "", errors.New("authentication failed: could not generate oauth token")
+}
+
+const _UrlBase = "https://android.clients.google.com"
+const _UrlFdfe = _UrlBase + "/fdfe"
+const _UrlAuth = _UrlBase + "/auth"
+const _UrlCheckIn = _UrlBase + "/checkin"
+const _UrlUploadDeviceConfig = _UrlFdfe + "/uploadDeviceConfig"
+
+var (
+   err_GPTokenExpired = errors.New("unauthorized, gp token expired")
+
+   httpClient = &http.Client{}
+)
