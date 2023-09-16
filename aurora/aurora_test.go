ab8ad66714ce58100e9cd54356f7e7c7b717beea
Steven Penny <srpen6+1268@gmail.com>
2023-09-16 10:38:24 -0500

    deleted:    blog/2023-09-16/aurora.go

--- aurora/aurora_test.go
+++ aurora/aurora_test.go
@@ -3,18 +3,28 @@ package aurora
 import (
    "154.pages.dev/http/option"
    "fmt"
+   "os"
    "testing"
 )
 
-func Test_Aurora(t *testing.T) {
+func Test_Marshal(t *testing.T) {
    option.No_Location()
    option.Verbose()
    var aurora aurora_OSS
-   text, err := aurora.MarshalText()
+   text, err := aurora.Marshal()
    if err != nil {
       t.Fatal(err)
    }
-   if err := aurora.UnmarshalText(text); err != nil {
+   os.WriteFile("aurora.json", text, 0666)
+}
+
+func Test_Unmarshal(t *testing.T) {
+   var aurora aurora_OSS
+   text, err := os.ReadFile("aurora.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   if err := aurora.Unmarshal(text); err != nil {
       t.Fatal(err)
    }
    fmt.Printf("%+v\n", aurora)
