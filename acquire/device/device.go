34b316661137c26b47babd6cfa1df097b6251a84
Steven Penny <srpen6+1268@gmail.com>
2023-09-16 14:06:18 -0500

    deleted:    acquire/acquire/acquire.go

--- acquire/device/device.go
+++ acquire/device/device.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-   "154.pages.dev/google/acquire"
-   "fmt"
+   "154.pages.dev/google/aurora"
+   "154.pages.dev/google/gplayapi"
+   "encoding/json"
+   "os"
 )
 
 func main() {
-   c, err := acquire.NewClientWithDeviceInfo()
+   var auth aurora.Aurora_OSS
+   {
+      b, err := os.ReadFile("aurora.json")
+      if err != nil {
+         panic(err)
+      }
+      if err := auth.Unmarshal(b); err != nil {
+         panic(err)
+      }
+   }
+   c, err := gplayapi.NewClient("srpen6@gmail.com", auth.Auth_Token)
    if err != nil {
       panic(err)
    }
-   fmt.Printf("%+v\n", c.AuthData.GsfID)
+   enc := json.NewEncoder(os.Stdout)
+   enc.SetIndent("", " ")
+   enc.Encode(c.AuthData)
 }
