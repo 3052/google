13f5575bd854baae847b31485a612bdaf84e8c3c
Steven Penny <srpen6+1268@gmail.com>
2023-09-16 21:45:17 -0500

    new file:   gplayapi/purchase_test.go

---
+++ gplayapi/purchase_test.go
@@ -0,0 +1,85 @@
+package gplayapi
+
+import (
+   "154.pages.dev/google/aurora"
+   "154.pages.dev/http/option"
+   "fmt"
+   "os"
+   "testing"
+   "time"
+)
+
+func Test_Acquire(t *testing.T) {
+   var auth aurora.Aurora_OSS
+   {
+      b, err := os.ReadFile("aurora.json")
+      if err != nil {
+         t.Fatal(err)
+      }
+      if err := auth.Unmarshal(b); err != nil {
+         t.Fatal(err)
+      }
+   }
+   client := GooglePlayClient{
+      AuthData: &AuthData{
+         AuthToken: auth.Auth_Token,
+         GsfID: "35f8806eb7f8a047",
+      },
+      DeviceInfo: Pixel3a,
+   }
+   for _, test := range x86_tests {
+      deliver, err := client.GetDeliveryResponse(test.doc, test.version)
+      if err != nil {
+         t.Fatal(err)
+      }
+      if deliver.AppDeliveryData == nil {
+         fmt.Println("ACQUIRE NEEDED, TRYING NOW:")
+         time.Sleep(time.Second)
+         err := client.AuthData.Acquire(test.doc, uint64(test.version))
+         if err != nil {
+            t.Fatal(err)
+         }
+         fmt.Println("sleep")
+         time.Sleep(9 * time.Second)
+         deliver, err := client.GetDeliveryResponse(test.doc, test.version)
+         if err != nil {
+            t.Fatal(err)
+         }
+         if deliver.AppDeliveryData != nil {
+            fmt.Println("ACQUIRE SUCCESS")
+         } else {
+            fmt.Println("ACQUIRE FAILURE")
+         }
+         return
+      }
+   }
+   fmt.Println("all apps acquired, run auth again")
+}
+
+func Test_Auth(t *testing.T) {
+   option.No_Location()
+   option.Verbose()
+   text, err := new(aurora.Aurora_OSS).Marshal()
+   if err != nil {
+      t.Fatal(err)
+   }
+   os.WriteFile("aurora.json", text, 0666)
+}
+
+var x86_tests = []struct{
+   doc string
+   version int
+}{
+   {"app.source.getcontact", 2739},
+   {"br.com.rodrigokolb.realdrum", 317},
+   {"com.amctve.amcfullepisodes", 28021790},
+   {"com.cabify.rider", 17144463},
+   {"com.clearchannel.iheartradio.controller", 710310201},
+   {"com.google.android.apps.walletnfcrel", 930739964},
+   {"com.google.android.youtube", 1539962304},
+   {"com.instagram.android", 370010808},
+   {"com.pinterest", 11338030},
+   {"kr.sira.metal", 74},
+   {"org.thoughtcrime.securesms", 132708},
+   {"org.videolan.vlc", 13050405},
+}
--- gplayapi/utils.go
+++ gplayapi/utils.go
@@ -1,80 +1,80 @@
 package gplayapi
 
 import (
-	"io"
-	"net/http"
-	"strings"
-	"google.golang.org/protobuf/proto"
+   "io"
+   "net/http"
+   "strings"
+   "google.golang.org/protobuf/proto"
 )
 
 import "154.pages.dev/google/gplayapi/gpproto"
 
+func doReq(r *http.Request) ([]byte, int, error) {
+   res, err := httpClient.Do(r)
+   if err != nil {
+      return nil, 0, err
+   }
+   defer res.Body.Close()
+   b, err := io.ReadAll(res.Body)
+   return b, res.StatusCode, err
+}
+
 func ptrBool(b bool) *bool {
-	return &b
+   return &b
 }
 
 func ptrStr(str string) *string {
-	return &str
+   return &str
 }
 
 func ptrInt32(i int32) *int32 {
-	return &i
-}
-
-func doReq(r *http.Request) ([]byte, int, error) {
-	res, err := httpClient.Do(r)
-	if err != nil {
-		return nil, 0, err
-	}
-	defer res.Body.Close()
-	b, err := io.ReadAll(res.Body)
-	return b, res.StatusCode, err
+   return &i
 }
 
 func parseResponse(res string) map[string]string {
-	ret := map[string]string{}
-	for _, ln := range strings.Split(res, "\n") {
-		keyVal := strings.SplitN(ln, "=", 2)
-		if len(keyVal) >= 2 {
-			ret[keyVal[0]] = keyVal[1]
-		}
-	}
-	return ret
+   ret := map[string]string{}
+   for _, ln := range strings.Split(res, "\n") {
+      keyVal := strings.SplitN(ln, "=", 2)
+      if len(keyVal) >= 2 {
+         ret[keyVal[0]] = keyVal[1]
+      }
+   }
+   return ret
 }
 
 func (client *GooglePlayClient) _doAuthedReq(r *http.Request) (_ *gpproto.Payload, err error) {
-	client.setDefaultHeaders(r)
-	b, status, err := doReq(r)
-	if err != nil {
-		return
-	}
-	if status == 401 {
-		return nil, err_GPTokenExpired
-	}
-	resp := &gpproto.ResponseWrapper{}
-	err = proto.Unmarshal(b, resp)
-	if err != nil {
-		return
-	}
-	return resp.Payload, nil
+   client.setDefaultHeaders(r)
+   b, status, err := doReq(r)
+   if err != nil {
+      return
+   }
+   if status == 401 {
+      return nil, err_GPTokenExpired
+   }
+   resp := &gpproto.ResponseWrapper{}
+   err = proto.Unmarshal(b, resp)
+   if err != nil {
+      return
+   }
+   return resp.Payload, nil
 }
 
 func (client *GooglePlayClient) doAuthedReq(r *http.Request) (res *gpproto.Payload, err error) {
-	res, err = client._doAuthedReq(r)
-	if err == err_GPTokenExpired {
-		err = client.RegenerateGPToken()
-		if err != nil {
-			return
-		}
-		if client.SessionFile != "" {
-			client.SaveSession(client.SessionFile)
-		}
-		res, err = client._doAuthedReq(r)
-	}
-	return
+   res, err = client._doAuthedReq(r)
+   if err == err_GPTokenExpired {
+      err = client.RegenerateGPToken()
+      if err != nil {
+         return
+      }
+      if client.SessionFile != "" {
+         client.SaveSession(client.SessionFile)
+      }
+      res, err = client._doAuthedReq(r)
+   }
+   return
 }
 
 func (client *GooglePlayClient) RegenerateGPToken() (err error) {
-	client.AuthData.AuthToken, err = client.GenerateGPToken()
-	return
+   client.AuthData.AuthToken, err = client.GenerateGPToken()
+   return
 }
