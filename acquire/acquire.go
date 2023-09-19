c95a96884cceaba4087c7707dd35b075e3e65ae3
Steven Penny <srpen6+1268@gmail.com>
2023-09-19 17:38:38 -0500

    +   "net/http/httputil"

--- acquire/acquire.go
+++ acquire/acquire.go
@@ -1,97 +1,57 @@
-package acquire
+package main
 
 import (
-   "154.pages.dev/google/play"
-   "154.pages.dev/protobuf"
-   "bytes"
-   "errors"
    "io"
    "net/http"
+   "net/http/httputil"
    "net/url"
+   "os"
    "strings"
 )
 
-const (
-   // android studio
-   // device_ID = "344d67278408e17a"
-   
-   // mine
-   device_ID = "30d95bf31df29aac"
-   
-   // aurora
-   //device_ID = "35cd340885ec81f3"
-)
-
-func Acquire(h *play.Header, doc string, version uint64) error {
-   body := protobuf.Message{
-      protobuf.Field{Number: 1, Type: 2, Value: protobuf.Prefix{
-         protobuf.Field{Number: 1, Type: 2, Value: protobuf.Prefix{
-            protobuf.Field{Number: 1, Type: 2,  Value: protobuf.Bytes(doc)},
-            protobuf.Field{Number: 2, Type: 0,  Value: protobuf.Varint(1)},
-            protobuf.Field{Number: 3, Type: 0,  Value: protobuf.Varint(3)},
-         }},
-         protobuf.Field{Number: 2, Type: 0,  Value: protobuf.Varint(1)},
-         protobuf.Field{Number: 3, Type: 2,  Value: protobuf.Bytes("")},
-      }},
-      protobuf.Field{Number: 8, Type: 2,  Value: protobuf.Bytes("")},
-      protobuf.Field{Number: 12, Type: 2, Value: protobuf.Prefix{
-         protobuf.Field{Number: 1, Type: 0,  Value: protobuf.Varint(version)},
-         protobuf.Field{Number: 3, Type: 0,  Value: protobuf.Varint(0)},
-      }},
-      protobuf.Field{Number: 13, Type: 0,  Value: protobuf.Varint(1)},
-      protobuf.Field{Number: 15, Type: 0,  Value: protobuf.Varint(0)},
-      protobuf.Field{Number: 22, Type: 2,  Value: protobuf.Bytes("nonce=z5qVwhnscgbdxNrBx-AsOD1TMGRpF_ohur_MQmR3JCYgaVqP8frLtCofKjPGpBbvbCERVPb7iMFt4entym6eKzsOICEX07ls-7wEITG51SaAa7GCgKXPkLd79hyFhI1FDWVZu5VPpdI9Oxlp_0WvMSVFwI6F5_sHpvI-tQEmOgB9YvTdfRJeyEXHG1da1nzzfAzE7_I38dLEmrHqwhSasb_td4qAceSkpvqvW8dv6NCarUL_Uf_yqJa_UoAjeAgSqKl4-1IgnE0_wQUbJt5CYyeUIrjVx4eSDN23uopFiV0bLR4uALifadia2Df53Fa04SVkwX23TB8T0M-K_L0GyA")},
-      protobuf.Field{Number: 25, Type: 0,  Value: protobuf.Varint(2)},
-      protobuf.Field{Number: 30, Type: 2, Value: protobuf.Prefix{
-         protobuf.Field{Number: 1, Type: 0,  Value: protobuf.Varint(2)},
-      }},
-   }
-   req := new(http.Request)
+func main() {
+   var req http.Request
    req.Header = make(http.Header)
+   req.Header["Accept-Encoding"] = []string{"gzip"}
+   req.Header["Accept-Language"] = []string{"en-US"}
+   req.Header["Authorization"] = []string{"Bearer ya29.a0AfB_byBFbblkPw8axrrH36QfGChD59B98T9SvEHUyT9qlXvp3_9bo2eo8o1xSNorKLE2Bow0nDFeMeUTkBNZH2e52N1ptv-2gCb0siAmzhONjHiBe7XvmkrszKepFqLHlE8NJuuSEQcGcpoxlZAh4x0WjhsgjqBwlV1svV3-CqjPm44kwH_yiWGWenAcHfM1KY9wgVXYst-VopZ1VZqYz_wfggktAZrjPSardJZ5t8lUoNr3DFZGNajTo_dVZ_ydlzr3pUMR1HGYfbIRxKtGpXKP29lKBBj-G_0hiSIOP0g8_21UHrjH7kGwYE84iDPNB4sRqAaCgYKAXUSARESFQGOcNnC64WSRP9UZTgXivBtLHHlpg0333"}
+   req.Header["Connection"] = []string{"Keep-Alive"}
+   req.Header["Content-Length"] = []string{"398"}
    req.Header["Content-Type"] = []string{"application/x-protobuf"}
-   req.Header["X-Dfe-Device-Id"] = []string{device_ID}
+   req.Header["Host"] = []string{"android.clients.google.com"}
+   req.Header["User-Agent"] = []string{"Android-Finsky/22.8.44-21%20%5B0%5D%20%5BPR%5D%20342964500 (api=3,versionCode=82284410,sdk=27,device=generic_x86,hardware=ranchu,product=sdk_gphone_x86,platformVersionRelease=8.1.0,model=Android%20SDK%20built%20for%20x86,buildId=OSM1.180201.037,isWideScreen=0,supportedAbis=x86)"}
+   req.Header["X-Dfe-Client-Id"] = []string{"am-unknown"}
+   req.Header["X-Dfe-Content-Filters"] = []string{""}
+   req.Header["X-Dfe-Cookie"] = []string{"EAEYACICVVMyUENqa2FOd29UTXprME5ETXdOekUzTmpjME9UVTVNemd6TlJJZ0NoQXhOamsxTVRZeU9UZ3hOVGt5T1RNMUVnd0k1Y1NvcUFZUTJQRGRtZ0k9ShIKAlVTEgwI5Zi8qQYQsNLC5AJYAA"}
+   req.Header["X-Dfe-Device-Config-Token"] = []string{"CjkaNwoTMzk0NDMwNzE3Njc0OTU5MzgzNRIgChAxNjk1MTYyOTgxNTkyOTM1EgwI5cSoqAYQ2PDdmgI="}
+   req.Header["X-Dfe-Device-Id"] = []string{"36bcfe7abb2ef4eb"}
+   req.Header["X-Dfe-Encoded-Targets"] = []string{"CAESD1elooEG2AOB4QGmNdE3ARp/4wUF0I+BBgnEAgQBAQ3nAX0vugFWASEKIw0iN4QBDgxjCVi9AYwCFExaARRbGeIFBJICPJwCpwekBFm9BpIK4AXgH64c7gbkB5saAgGQBaUHpQLtCvcHqwLjCYMDvALLARakP58/gwZF5QXUAq4R6RqIDvYL9wKbA8c7wcaQEg"}
+   req.Header["X-Dfe-Mccmnc"] = []string{"310260"}
+   req.Header["X-Dfe-Network-Type"] = []string{"4"}
+   req.Header["X-Dfe-Request-Params"] = []string{"timeoutMs=35000"}
+   req.Header["X-Public-Android-Id"] = []string{"c347e6c1b1c92e4f"}
    req.Method = "POST"
    req.ProtoMajor = 1
    req.ProtoMinor = 1
    req.URL = new(url.URL)
-   req.URL.Host = "play-fe.googleapis.com"
+   req.URL.Host = "android.clients.google.com"
    req.URL.Path = "/fdfe/acquire"
+   req.URL.RawPath = ""
    val := make(url.Values)
    val["theme"] = []string{"2"}
    req.URL.RawQuery = val.Encode()
    req.URL.Scheme = "https"
-   req.Body = io.NopCloser(bytes.NewReader(body.Append(nil)))
-   req.Header.Set(h.Authorization())
-   res, err := http.DefaultClient.Do(req)
+   req.Body = io.NopCloser(req_body)
+   res, err := new(http.Transport).RoundTrip(&req)
    if err != nil {
-      return err
+      panic(err)
    }
    defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      return errors.New(res.Status)
+   res_body, err := httputil.DumpResponse(res, true)
+   if err != nil {
+      panic(err)
    }
-   {
-      b, err := io.ReadAll(res.Body)
-      if err != nil {
-         return err
-      }
-      s := string(b)
-      for _, err := range acquire_errors {
-         if strings.Contains(s, err) {
-            return new_error(err)
-         }
-      }
-   }
-   return nil
+   os.Stdout.Write(res_body)
 }
 
-var acquire_errors = []string{
-   "Error",
-   "Please open my apps to establish a connection with the server.",
-   "error",
-}
-
-func new_error(s string) error {
-   s = strings.TrimSuffix(s, ".")
-   return errors.New(strings.ToLower(s))
-}
+var req_body = strings.NewReader("\n\x18\n\x12\n\fcom.duolingo\x10\x01\x18\x03\x10\x01\x1a\x00\"\x02\b\nB\x00b\x05\b\xa4\r\x18\x00h\x01x\x00\xb2\x01\xdc\x02nonce=LHEvHQSIv40zqfHYASj1rKZuAlwtyacUe2pe1tNd9mNv0suSHagkp-88-WaZC48Ev8JTtj9AsH_w6waS7IMFUlrOtxQ70udW-oUjACf7qMlgGnoMLTS2h5pOb_n327LSWmX2lnOoSIMjw6Z1bi5DkPGPrxzPDCnJQfxjke_XvsScFbFqoAdDegtrxKViI0r6dOC3KtcaavBPnNBOZa8DH6RY8Dg83QxzAgIvm7zyUSgrAJIBPErFoR3uDnnb_6nKPV5w-PFat1BgOupbCWAoaO0HTmHaAuE51PcafSGP0DC-D_dlDiJrwbHq8J2F8-3MeHWUwxHmJ1NVhTXYM_njvQ\xc8\x01\x02")
--- acquire/delivery.go
+++ acquire/delivery.go
@@ -1,73 +1,63 @@
-package acquire
+package main
 
 import (
-   "154.pages.dev/google/play"
-   "154.pages.dev/protobuf"
-   "errors"
-   //"fmt"
    "io"
    "net/http"
+   "net/http/httputil"
    "net/url"
-   "strconv"
+   "os"
+   "strings"
 )
 
-func Delivery(h *play.Header, doc string, version uint64) error {
-   req := new(http.Request)
-   req.Method = "GET"
+func main() {
+   var req http.Request
    req.Header = make(http.Header)
+   req.Header["Accept-Encoding"] = []string{"gzip"}
+   req.Header["Accept-Language"] = []string{"en-US"}
+   req.Header["Authorization"] = []string{"Bearer ya29.a0AfB_byBFbblkPw8axrrH36QfGChD59B98T9SvEHUyT9qlXvp3_9bo2eo8o1xSNorKLE2Bow0nDFeMeUTkBNZH2e52N1ptv-2gCb0siAmzhONjHiBe7XvmkrszKepFqLHlE8NJuuSEQcGcpoxlZAh4x0WjhsgjqBwlV1svV3-CqjPm44kwH_yiWGWenAcHfM1KY9wgVXYst-VopZ1VZqYz_wfggktAZrjPSardJZ5t8lUoNr3DFZGNajTo_dVZ_ydlzr3pUMR1HGYfbIRxKtGpXKP29lKBBj-G_0hiSIOP0g8_21UHrjH7kGwYE84iDPNB4sRqAaCgYKAXUSARESFQGOcNnC64WSRP9UZTgXivBtLHHlpg0333"}
+   req.Header["Connection"] = []string{"Keep-Alive"}
+   req.Header["Content-Length"] = []string{"0"}
+   req.Header["Host"] = []string{"android.clients.google.com"}
    req.Header["User-Agent"] = []string{"Android-Finsky/22.8.44-21%20%5B0%5D%20%5BPR%5D%20342964500 (api=3,versionCode=82284410,sdk=27,device=generic_x86,hardware=ranchu,product=sdk_gphone_x86,platformVersionRelease=8.1.0,model=Android%20SDK%20built%20for%20x86,buildId=OSM1.180201.037,isWideScreen=0,supportedAbis=x86)"}
-   req.Header["X-Dfe-Device-Id"] = []string{device_ID}
+   req.Header["X-Dfe-Client-Id"] = []string{"am-unknown"}
+   req.Header["X-Dfe-Content-Filters"] = []string{""}
+   req.Header["X-Dfe-Cookie"] = []string{"EAEYACICVVMyUENqa2FOd29UTXprME5ETXdOekUzTmpjME9UVTVNemd6TlJJZ0NoQXhOamsxTVRZeU9UZ3hOVGt5T1RNMUVnd0k1Y1NvcUFZUTJQRGRtZ0k9ShIKAlVTEgwI5Zi8qQYQsNLC5AJYAA"}
+   req.Header["X-Dfe-Device-Config-Token"] = []string{"CjkaNwoTMzk0NDMwNzE3Njc0OTU5MzgzNRIgChAxNjk1MTYyOTgxNTkyOTM1EgwI5cSoqAYQ2PDdmgI="}
+   req.Header["X-Dfe-Device-Id"] = []string{"36bcfe7abb2ef4eb"}
+   req.Header["X-Dfe-Encoded-Targets"] = []string{"CAESD1elooEG2AOB4QGmNdE3ARp/4wUF0I+BBgnEAgQBAQ3nAX0vugFWASEKIw0iN4QBDgxjCVi9AYwCFExaARRbGeIFBJICPJwCpwekBFm9BpIK4AXgH64c7gbkB5saAgGQBaUHpQLtCvcHqwLjCYMDvALLARakP58/gwZF5QXUAq4R6RqIDvYL9wKbA8c7wcaQEg"}
+   req.Header["X-Dfe-Mccmnc"] = []string{"310260"}
+   req.Header["X-Dfe-Network-Type"] = []string{"4"}
+   req.Header["X-Dfe-Request-Params"] = []string{"timeoutMs=4000"}
+   req.Header["X-Dfe-Userlanguages"] = []string{"en_US"}
+   req.Method = "GET"
    req.ProtoMajor = 1
    req.ProtoMinor = 1
    req.URL = new(url.URL)
-   req.URL.Host = "play-fe.googleapis.com"
+   req.URL.Host = "android.clients.google.com"
    req.URL.Path = "/fdfe/delivery"
+   req.URL.RawPath = ""
    val := make(url.Values)
-   req.URL.Scheme = "https"
-   req.Header.Set(h.Authorization())
-   val["doc"] = []string{doc}
-   val["vc"] = []string{strconv.FormatUint(version, 10)}
-   //val["da"] = []string{"3"}
-   //val["fdcf"] = []string{"1", "2"}
-   //val["ia"] = []string{"false"}
-   //val["isid"] = []string{"oLHwp38pRtqEky5tTkuimg"}
-   //val["ot"] = []string{"1"}
-   //val["st"] = []string{"EMbEnagGGdo8npHYQdlB"}
+   val["da"] = []string{"3"}
+   val["doc"] = []string{"com.duolingo"}
+   val["fdcf"] = []string{"1", "2"}
+   val["ia"] = []string{"false"}
+   val["isid"] = []string{"sOSbXrwaQ-WUNeCgghpZ3w"}
+   val["ot"] = []string{"1"}
+   val["st"] = []string{"EPPEqKgGGWgF45yIQtlB"}
+   val["vc"] = []string{"1700"}
    req.URL.RawQuery = val.Encode()
-   res, err := http.DefaultClient.Do(req)
+   req.URL.Scheme = "https"
+   req.Body = nil
+   res, err := new(http.Transport).RoundTrip(&req)
    if err != nil {
-      return err
+      panic(err)
    }
    defer res.Body.Close()
-   data, err := io.ReadAll(res.Body)
+   res_body, err := httputil.DumpResponse(res, true)
    if err != nil {
-      return err
+      panic(err)
    }
-   mes, err := protobuf.Consume(data) // ResponseWrapper
-   if err != nil {
-      return err
-   }
-   //fmt.Printf("%#v\n", mes)
-   mes, ok := mes.Message(1)
-   if !ok {
-      return errors.New("payload")
-   }
-   mes, ok = mes.Message(21)
-   if !ok {
-      return errors.New("deliveryResponse")
-   }
-   status, ok := mes.Varint(1)
-   if ok {
-      switch status {
-      case 3:
-         return errors.New("acquire")
-      case 5:
-         return errors.New("version")
-      }
-   }
-   _, ok = mes.Message(2)
-   if !ok {
-      return errors.New("appDeliveryData")
-   }
-   return nil
+   os.Stdout.Write(res_body)
 }
+
+var req_body = strings.NewReader("")
