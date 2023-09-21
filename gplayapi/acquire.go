2063ac1980f0ccc363646328044955abca98da2e
Steven Penny <srpen6+1268@gmail.com>
2023-09-21 18:09:09 -0500

    deleted:    gplayapi/utils.go

--- gplayapi/acquire.go
+++ gplayapi/acquire.go
@@ -1,14 +1,94 @@
 package gplayapi
 
 import (
+   "154.pages.dev/google/gplayapi/gpproto"
    "154.pages.dev/protobuf"
    "bytes"
+   "errors"
    "fmt"
+   "google.golang.org/protobuf/proto"
    "io"
    "net/http"
    "net/url"
+   "strings"
 )
 
+//goland:noinspection GoUnusedConst
+const (
+   ImageTypeAppScreenshot = iota + 1
+   ImageTypePlayStorePageBackground
+   ImageTypeYoutubeVideoLink
+   ImageTypeAppIcon
+   ImageTypeCategoryIcon
+   ImageTypeYoutubeVideoThumbnail = 13
+
+   UrlBase               = "https://android.clients.google.com"
+   UrlFdfe               = UrlBase + "/fdfe"
+   UrlAuth               = UrlBase + "/auth"
+   UrlCheckIn            = UrlBase + "/checkin"
+   UrlDetails            = UrlFdfe + "/details"
+   UrlDelivery           = UrlFdfe + "/delivery"
+   UrlPurchase           = UrlFdfe + "/purchase"
+   UrlToc                = UrlFdfe + "/toc"
+   UrlTosAccept          = UrlFdfe + "/acceptTos"
+   UrlUploadDeviceConfig = UrlFdfe + "/uploadDeviceConfig"
+)
+
+var ErrNilPayload = errors.New("got nil payload from google play")
+
+func ptrBool(b bool) *bool {
+   return &b
+}
+
+func ptrStr(str string) *string {
+   return &str
+}
+
+func ptrInt32(i int32) *int32 {
+   return &i
+}
+
+func doReq(r *http.Request) ([]byte, int, error) {
+   res, err := httpClient.Do(r)
+   if err != nil {
+      return nil, 0, err
+   }
+   defer res.Body.Close()
+   b, err := io.ReadAll(res.Body)
+   return b, res.StatusCode, err
+}
+
+func parseResponse(res string) map[string]string {
+   ret := map[string]string{}
+   for _, ln := range strings.Split(res, "\n") {
+      keyVal := strings.SplitN(ln, "=", 2)
+      if len(keyVal) >= 2 {
+         ret[keyVal[0]] = keyVal[1]
+      }
+   }
+   return ret
+}
+
+func (client *GooglePlayClient) _doAuthedReq(r *http.Request) (_ *gpproto.Payload, err error) {
+   client.setDefaultHeaders(r)
+   b, status, err := doReq(r)
+   if err != nil {
+      return
+   }
+   if status == 401 {
+      return nil, err_GPTokenExpired
+   }
+   resp := &gpproto.ResponseWrapper{}
+   err = proto.Unmarshal(b, resp)
+   if err != nil {
+      return
+   }
+   return resp.Payload, nil
+}
+
+func (client *GooglePlayClient) doAuthedReq(r *http.Request) (res *gpproto.Payload, err error) {
+   return client._doAuthedReq(r)
+}
 func (g GooglePlayClient) Acquire(doc string, version uint64) error {
    var req http.Request
    req.Header = make(http.Header)
