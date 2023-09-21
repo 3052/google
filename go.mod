44dd22c275507c95bfe1502cb350e63a60e82e94
Steven Penny <srpen6+1268@gmail.com>
2023-09-21 17:20:37 -0500

    deleted:    gplayapi/constants.go

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module 154.pages.dev/google
 go 1.21.0
 
 require (
-	154.pages.dev/http v1.1.4
+	154.pages.dev/http v1.1.5
 	154.pages.dev/protobuf v1.0.0
-	154.pages.dev/strconv v1.0.4
+	154.pages.dev/strconv v1.0.5
 )
 
-require google.golang.org/protobuf v1.31.0 // indirect
+require google.golang.org/protobuf v1.31.0
--- gplayapi/client_test.go
+++ gplayapi/client_test.go
@@ -3,10 +3,12 @@ package gplayapi
 import (
    "154.pages.dev/http/option"
    "encoding/json"
+   "fmt"
    "net/url"
    "os"
    "strconv"
    "testing"
+   "time"
 )
 
 const query = "doc=com.zzkko&vc=818"
@@ -16,7 +18,12 @@ func Test_Client(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   enc := json.NewEncoder(os.Stdout)
+   file, err := os.Create("client.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer file.Close()
+   enc := json.NewEncoder(file)
    enc.SetIndent("", " ")
    enc.Encode(client)
 }
@@ -39,20 +46,11 @@ func Test_Acquire(t *testing.T) {
    if err := client.Acquire(doc, uint64(version)); err != nil {
       t.Fatal(err)
    }
+   fmt.Println("sleep")
+   time.Sleep(9*time.Second)
 }
 
-func parse(s string) (string, int, error) {
-   q, err := url.ParseQuery(s)
-   if err != nil {
-      return "", 0, err
-   }
-   v, err := strconv.Atoi(q.Get("vc"))
-   if err != nil {
-      return "", 0, err
-   }
-   return q.Get("doc"), v, nil
-}
-
+// case 3: return nil, errors.New("acquire")
 func Test_Delivery(t *testing.T) {
    text, err := os.ReadFile("client.json")
    if err != nil {
@@ -73,4 +71,16 @@ func Test_Delivery(t *testing.T) {
    enc := json.NewEncoder(os.Stdout)
    enc.SetIndent("", " ")
    enc.Encode(delivery)
+}
+
+func parse(s string) (string, int, error) {
+   q, err := url.ParseQuery(s)
+   if err != nil {
+      return "", 0, err
+   }
+   v, err := strconv.Atoi(q.Get("vc"))
+   if err != nil {
+      return "", 0, err
+   }
+   return q.Get("doc"), v, nil
 }
