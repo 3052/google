2be6fdcc1ad68061c75361f2120bab61fb71aa5b
Steven Penny <srpen6+1268@gmail.com>
2023-09-24 10:31:42 -0500

    deleted:    gplayapi/client.go

--- gplayapi/devices.go
+++ gplayapi/devices.go
@@ -13,49 +13,6 @@ var (
 	features              = []string{"android.hardware.audio.low_latency", "android.hardware.audio.output", "android.hardware.audio.pro", "android.hardware.bluetooth", "android.hardware.bluetooth_le", "android.hardware.camera", "android.hardware.camera.any", "android.hardware.camera.autofocus", "android.hardware.camera.capability.manual_post_processing", "android.hardware.camera.capability.manual_sensor", "android.hardware.camera.capability.raw", "android.hardware.camera.flash", "android.hardware.camera.front", "android.hardware.camera.level.full", "android.hardware.faketouch", "android.hardware.fingerprint", "android.hardware.location", "android.hardware.location.gps", "android.hardware.location.network", "android.hardware.microphone", "android.hardware.nfc", "android.hardware.nfc.any", "android.hardware.nfc.hce", "android.hardware.nfc.hcef", "android.hardware.opengles.aep", "android.hardware.ram.normal", "android.hardware.screen.landscape", "android.hardware.screen.portrait", "android.hardware.sensor.accelerometer", "android.hardware.sensor.assist", "android.hardware.sensor.barometer", "android.hardware.sensor.compass", "android.hardware.sensor.gyroscope", "android.hardware.sensor.hifi_sensors", "android.hardware.sensor.light", "android.hardware.sensor.proximity", "android.hardware.sensor.stepcounter", "android.hardware.sensor.stepdetector", "android.hardware.strongbox_keystore", "android.hardware.telephony", "android.hardware.telephony.carrierlock", "android.hardware.telephony.cdma", "android.hardware.telephony.euicc", "android.hardware.telephony.gsm", "android.hardware.touchscreen", "android.hardware.touchscreen.multitouch", "android.hardware.touchscreen.multitouch.distinct", "android.hardware.touchscreen.multitouch.jazzhand", "android.hardware.usb.accessory", "android.hardware.usb.host", "android.hardware.vulkan.compute", "android.hardware.vulkan.level", "android.hardware.vulkan.version", "android.hardware.wifi", "android.hardware.wifi.aware", "android.hardware.wifi.direct", "android.hardware.wifi.passpoint", "android.hardware.wifi.rtt", "android.software.activities_on_secondary_displays", "android.software.app_widgets", "android.software.autofill", "android.software.backup", "android.software.cant_save_state", "android.software.companion_device_setup", "android.software.connectionservice", "android.software.cts", "android.software.device_admin", "android.software.device_id_attestation", "android.software.file_based_encryption", "android.software.home_screen", "android.software.input_methods", "android.software.live_wallpaper", "android.software.managed_users", "android.software.midi", "android.software.picture_in_picture", "android.software.print", "android.software.securely_removes_users", "android.software.sip", "android.software.sip.voip", "android.software.verified_boot", "android.software.voice_recognizers", "android.software.webview", "com.google.android.apps.dialer.SUPPORTED", "com.google.android.apps.photos.PIXEL_2019_MIDYEAR_PRELOAD", "com.google.android.feature.EXCHANGE_6_2", "com.google.android.feature.GOOGLE_BUILD", "com.google.android.feature.GOOGLE_EXPERIENCE", "com.google.android.feature.PIXEL_2017_EXPERIENCE", "com.google.android.feature.PIXEL_2018_EXPERIENCE", "com.google.android.feature.PIXEL_2019_MIDYEAR_EXPERIENCE", "com.google.android.feature.PIXEL_EXPERIENCE", "com.google.android.feature.TURBO_PRELOAD", "com.google.android.feature.WELLBEING", "com.google.android.feature.ZERO_TOUCH", "com.google.hardware.camera.easel_2018", "com.verizon.hardware.telephony.ehrpd", "com.verizon.hardware.telephony.lte"}
 	locales               = []string{"af", "af_ZA", "am", "am_ET", "ar", "ar_EG", "ar_SA", "ar_XB", "as", "ast", "az", "be", "be_BY", "bg", "bg_BG", "bh_IN", "bn", "bs", "ca", "ca_ES", "cs", "cs_CZ", "cy_GB", "da", "da_DK", "de", "de_DE", "el", "el_GR", "en", "en_AU", "en_CA", "en_GB", "en_IN", "en_US", "en_XA", "es", "es_ES", "es_US", "et", "et_EE", "eu", "fa", "fa_IR", "fi", "fi_FI", "fil", "fil_PH", "fr", "fr_CA", "fr_FR", "gl", "gl_ES", "gu", "hi", "hi_IN", "hr", "hr_HR", "hu", "hu_HU", "hy", "in", "in_ID", "is", "it", "it_IT", "iw", "iw_IL", "ja", "ja_JP", "ka", "kab_DZ", "kk", "km", "kn", "ko", "ko_KR", "ky", "lo", "lt", "lt_LT", "lv", "lv_LV", "mk", "ml", "mn", "mr", "ms", "ms_MY", "my", "nb", "nb_NO", "ne", "nl", "nl_NL", "or", "pa", "pa_IN", "pl", "pl_PL", "pt", "pt_BR", "pt_PT", "ro", "ro_RO", "ru", "ru_RU", "sc_IT", "si", "sk", "sk_SK", "sl", "sl_SI", "sq", "sr", "sr_Latn", "sr_RS", "sv", "sv_SE", "sw", "sw_TZ", "ta", "te", "th", "th_TH", "tr", "tr_TR", "uk", "uk_UA", "ur", "uz", "vi", "vi_VN", "zh_CN", "zh_HK", "zh_TW", "zu", "zu_ZA"}
 
-	sargo = ptrStr("sargo")
-
-	// Pixel3a is default device which uses arm64 and Android 9 / SDK 28
-	Pixel3a = &DeviceInfo{
-		Build: &DeviceBuildInfo{
-			AndroidBuildProto: &gpproto.AndroidBuildProto{
-				BuildProduct:   sargo,
-				Radio:          ptrStr("g670-00011-190411-B-5457439"),
-				Bootloader:     ptrStr("b4s4-0.1-5613380"),
-				Carrier:        ptrStr("google"),
-				Device:         sargo,
-				Model:          ptrStr("Pixel 3a"),
-				Manufacturer:   ptrStr("google"),
-				Product:        sargo,
-				Id:             ptrStr("PQ3B.190705.003"),
-				SdkVersion:     ptrInt32(28),
-				Client:         androidGoogle,
-				GoogleServices: ptrInt32(203615028),
-				OtaInstalled:   falsePtr,
-			},
-			VersionRelease: 9,
-		},
-		SimOperator:  "20815",
-		Platforms:    []string{"arm64-v8a", "armeabi-v7a", "armeabi"},
-		CellOperator: "334050",
-		Roaming:      roaming,
-		TimeZone:     "America/Mexico_City",
-		TouchScreen:  3,
-		Keyboard:     1,
-		Navigation:   1,
-		ScreenLayout: 2,
-		Screen: &DeviceInfoScreen{
-			Density: 490,
-			Width:   1080,
-			Height:  2073,
-		},
-		GLVersion:       glVersion,
-		GLExtensions:    glExtensions,
-		SharedLibraries: sharedLibraries,
-		Features:        features,
-		Locales:         locales,
-	}
-
 	santoni = ptrStr("santoni")
 
 	// Redmi4 is device which uses arm and Android 10 / SDK 29
--- gplayapi/utils.go
+++ gplayapi/utils.go
@@ -2,15 +2,77 @@ package gplayapi
 
 import (
    "154.pages.dev/google/gplayapi/gpproto"
+   "encoding/json"
    "errors"
    "google.golang.org/protobuf/proto"
    "io"
    "net/http"
    "net/url"
+   "os"
    "strconv"
    "strings"
 )
 
+type GooglePlayClient struct {
+   AuthData   *AuthData
+   DeviceInfo *DeviceInfo
+
+   // SessionFile if SessionFile is set then session will be saved to it after modification
+   SessionFile string
+}
+
+var (
+   err_GPTokenExpired = errors.New("unauthorized, gp token expired")
+
+   httpClient = &http.Client{}
+)
+
+func NewClientWithDeviceInfo(email, aasToken string, deviceInfo *DeviceInfo) (client *GooglePlayClient, err error) {
+   authData := &AuthData{
+      Email:    email,
+      AASToken: aasToken,
+      Locale:   "en_GB",
+   }
+   client = &GooglePlayClient{AuthData: authData, DeviceInfo: deviceInfo}
+
+   _, err = client.GenerateGsfID()
+   if err != nil {
+      return
+   }
+
+   deviceConfigRes, err := client.uploadDeviceConfig()
+   if err != nil {
+      return
+   }
+   authData.DeviceConfigToken = deviceConfigRes.GetUploadDeviceConfigToken()
+
+   token, err := client.GenerateGPToken()
+   if err != nil {
+      return
+   }
+   authData.AuthToken = token
+
+   _, err = client.toc()
+   return
+}
+
+func (client *GooglePlayClient) SaveSession(file string) error {
+   f, err := os.OpenFile(file, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+   if err != nil {
+      return err
+   }
+   return json.NewEncoder(f).Encode(client.AuthData)
+}
+
+func LoadSessionWithDeviceInfo(file string, deviceInfo *DeviceInfo) (client *GooglePlayClient, err error) {
+   f, err := os.Open(file)
+   if err != nil {
+      return
+   }
+   client = &GooglePlayClient{DeviceInfo: deviceInfo}
+   err = json.NewDecoder(f).Decode(&client.AuthData)
+   return
+}
 var ErrMissingAppDeliveryData = errors.New("buy response is missing AppDeliveryData")
 
 func (client *GooglePlayClient) GetDeliveryResponse(packageName string, version int) (*gpproto.DeliveryResponse, error) {
