9598fb726818824fcd664f753f8b0af90a95ba51
Steven Penny <spen+3052@protonmail.com>
2023-10-22 20:58:32 -0500

    modified:   play/play_test.go

--- play/details_test.go
+++ play/details_test.go
@@ -1,77 +1,77 @@
 package play
 
 import (
-	"fmt"
-	"os"
-	"testing"
-	"time"
+   "fmt"
+   "os"
+   "testing"
+   "time"
 )
 
 func Test_Details(t *testing.T) {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		t.Fatal(err)
-	}
-	home += "/google/play/"
-	var head Header
-	b, err := os.ReadFile(home + "token.txt")
-	if err != nil {
-		t.Fatal(err)
-	}
-	head.Set_Authorization(b)
-	head.Set_Agent(false)
-	for i, app := range apps {
-		b, err := os.ReadFile(home + Platforms[app.platform] + ".bin")
-		if err != nil {
-			t.Fatal(err)
-		}
-		head.Set_Device(b)
-		d, err := head.Details(app.doc)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if _, ok := d.Downloads(); !ok {
-			t.Fatal(err)
-		}
-		if f := d.Files(); len(f) == 0 {
-			t.Fatal(f)
-		}
-		if _, ok := d.Name(); !ok {
-			t.Fatal("name")
-		}
-		if _, ok := d.Offered_By(); !ok {
-			t.Fatal("offered by")
-		}
-		if _, ok := d.Price(); !ok {
-			t.Fatal("price")
-		}
-		if _, ok := d.Price_Currency(); !ok {
-			t.Fatal("price currency")
-		}
-		if _, ok := d.Requires(); !ok {
-			t.Fatal("requires")
-		}
-		if _, ok := d.Size(); !ok {
-			t.Fatal("size")
-		}
-		{
-			s, ok := d.Updated_On()
-			if !ok {
-				t.Fatal("updated on")
-			}
-			d, err := time.Parse("Jan 2, 2006", s)
-			if err != nil {
-				t.Fatal(err)
-			}
-			apps[i].date = d.Format("2006-01-02")
-		}
-		if _, ok := d.Version_Code(); !ok {
-			t.Fatal("version code")
-		}
-		if _, ok := d.Version_Name(); !ok {
-			t.Fatal("version name")
-		}
-		fmt.Printf("%#v,\n", app)
-		time.Sleep(99 * time.Millisecond)
-	}
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   home += "/google/play/"
+   var head Header
+   b, err := os.ReadFile(home + "token.txt")
+   if err != nil {
+      t.Fatal(err)
+   }
+   head.Set_Authorization(b)
+   head.Set_Agent(false)
+   for i, app := range apps {
+      b, err := os.ReadFile(home + Platforms[app.platform] + ".bin")
+      if err != nil {
+         t.Fatal(err)
+      }
+      head.Set_Device(b)
+      d, err := head.Details(app.doc)
+      if err != nil {
+         t.Fatal(err)
+      }
+      if _, ok := d.Downloads(); !ok {
+         t.Fatal(err)
+      }
+      if f := d.Files(); len(f) == 0 {
+         t.Fatal(f)
+      }
+      if _, ok := d.Name(); !ok {
+         t.Fatal("name")
+      }
+      if _, ok := d.Offered_By(); !ok {
+         t.Fatal("offered by")
+      }
+      if _, ok := d.Price(); !ok {
+         t.Fatal("price")
+      }
+      if _, ok := d.Price_Currency(); !ok {
+         t.Fatal("price currency")
+      }
+      if _, ok := d.Requires(); !ok {
+         t.Fatal("requires")
+      }
+      if _, ok := d.Size(); !ok {
+         t.Fatal("size")
+      }
+      {
+         s, ok := d.Updated_On()
+         if !ok {
+            t.Fatal("updated on")
+         }
+         d, err := time.Parse("Jan 2, 2006", s)
+         if err != nil {
+            t.Fatal(err)
+         }
+         apps[i].date = d.Format("2006-01-02")
+      }
+      if _, ok := d.Version_Code(); !ok {
+         t.Fatal("version code")
+      }
+      if _, ok := d.Version_Name(); !ok {
+         t.Fatal("version name")
+      }
+      fmt.Printf("%#v,\n", app)
+      time.Sleep(99 * time.Millisecond)
+   }
 }
--- play/play_test.go
+++ play/play_test.go
@@ -3,47 +3,47 @@ package play
 import "strconv"
 
 var apps = []app_type{
-	{date: "2023-02-01", requires: 24, platform: 1, doc: "com.miui.weather2"},
-	{date: "2023-02-20", requires: 17, platform: 0, doc: "org.videolan.vlc"},
-	{date: "2023-04-12", requires: 21, platform: 0, doc: "com.amctve.amcfullepisodes"},
-	{date: "2023-05-08", requires: 23, platform: 0, doc: "br.com.rodrigokolb.realdrum"},
-	{date: "2023-06-23", requires: 23, platform: 1, doc: "com.sygic.aura"},
-	{date: "2023-06-30", requires: 24, platform: 1, doc: "com.supercell.brawlstars"},
-	{date: "2023-07-28", requires: 21, platform: 0, doc: "kr.sira.metal"},
-	{date: "2023-08-04", requires: 21, platform: 2, doc: "com.miHoYo.GenshinImpact"},
-	{date: "2023-08-04", requires: 23, platform: 1, doc: "com.app.xt"},
-	{date: "2023-08-08", requires: 24, platform: 0, doc: "com.google.android.apps.walletnfcrel"},
-	{date: "2023-08-09", requires: 23, platform: 1, doc: "com.madhead.tos.zh"},
-	{date: "2023-08-10", requires: 21, platform: 2, doc: "com.kakaogames.twodin"},
-	{date: "2023-08-10", requires: 23, platform: 0, doc: "com.clearchannel.iheartradio.controller"},
-	{date: "2023-08-15", requires: 26, platform: 0, doc: "app.source.getcontact"},
-	{date: "2023-08-18", requires: 21, platform: 1, doc: "com.xiaomi.smarthome"},
-	{date: "2023-08-21", requires: 21, platform: 0, doc: "com.cabify.rider"},
-	{date: "2023-08-21", requires: 21, platform: 1, doc: "com.binance.dev"},
-	{date: "2023-08-21", requires: 24, platform: 0, doc: "com.instagram.android"},
-	{date: "2023-08-21", requires: 26, platform: 0, doc: "com.google.android.youtube"},
-	{date: "2023-08-22", requires: 21, platform: 0, doc: "org.thoughtcrime.securesms"},
-	{date: "2023-08-22", requires: 24, platform: 0, doc: "com.pinterest"},
-	{date: "2023-08-24", requires: 23, platform: 1, doc: "com.axis.drawingdesk.v3"},
+   {date: "2023-02-01", requires: 24, platform: 1, doc: "com.miui.weather2"},
+   {date: "2023-02-20", requires: 17, platform: 0, doc: "org.videolan.vlc"},
+   {date: "2023-04-12", requires: 21, platform: 0, doc: "com.amctve.amcfullepisodes"},
+   {date: "2023-05-08", requires: 23, platform: 0, doc: "br.com.rodrigokolb.realdrum"},
+   {date: "2023-06-23", requires: 23, platform: 1, doc: "com.sygic.aura"},
+   {date: "2023-06-30", requires: 24, platform: 1, doc: "com.supercell.brawlstars"},
+   {date: "2023-07-28", requires: 21, platform: 0, doc: "kr.sira.metal"},
+   {date: "2023-08-04", requires: 21, platform: 2, doc: "com.miHoYo.GenshinImpact"},
+   {date: "2023-08-04", requires: 23, platform: 1, doc: "com.app.xt"},
+   {date: "2023-08-08", requires: 24, platform: 0, doc: "com.google.android.apps.walletnfcrel"},
+   {date: "2023-08-09", requires: 23, platform: 1, doc: "com.madhead.tos.zh"},
+   {date: "2023-08-10", requires: 21, platform: 2, doc: "com.kakaogames.twodin"},
+   {date: "2023-08-10", requires: 23, platform: 0, doc: "com.clearchannel.iheartradio.controller"},
+   {date: "2023-08-15", requires: 26, platform: 0, doc: "app.source.getcontact"},
+   {date: "2023-08-18", requires: 21, platform: 1, doc: "com.xiaomi.smarthome"},
+   {date: "2023-08-21", requires: 21, platform: 0, doc: "com.cabify.rider"},
+   {date: "2023-08-21", requires: 21, platform: 1, doc: "com.binance.dev"},
+   {date: "2023-08-21", requires: 24, platform: 0, doc: "com.instagram.android"},
+   {date: "2023-08-21", requires: 26, platform: 0, doc: "com.google.android.youtube"},
+   {date: "2023-08-22", requires: 21, platform: 0, doc: "org.thoughtcrime.securesms"},
+   {date: "2023-08-22", requires: 24, platform: 0, doc: "com.pinterest"},
+   {date: "2023-08-24", requires: 23, platform: 1, doc: "com.axis.drawingdesk.v3"},
 }
 
 func (a app_type) GoString() string {
-	var b []byte
-	b = append(b, "{date:"...)
-	b = strconv.AppendQuote(b, a.date)
-	b = append(b, ",requires:"...)
-	b = strconv.AppendInt(b, a.requires, 10)
-	b = append(b, ",platform:"...)
-	b = strconv.AppendInt(b, a.platform, 10)
-	b = append(b, ",doc:"...)
-	b = strconv.AppendQuote(b, a.doc)
-	b = append(b, '}')
-	return string(b)
+   var b []byte
+   b = append(b, "{date:"...)
+   b = strconv.AppendQuote(b, a.date)
+   b = append(b, ",requires:"...)
+   b = strconv.AppendInt(b, a.requires, 10)
+   b = append(b, ",platform:"...)
+   b = strconv.AppendInt(b, a.platform, 10)
+   b = append(b, ",doc:"...)
+   b = strconv.AppendQuote(b, a.doc)
+   b = append(b, '}')
+   return string(b)
 }
 
 type app_type struct {
-	date     string
-	requires int64
-	platform int64 // X-DFE-Device-ID
-	doc      string
+   date     string
+   requires int64
+   platform int64 // X-DFE-Device-ID
+   doc      string
 }
