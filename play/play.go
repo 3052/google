162553419f922f76f5ea9e83bc4787c859a94f7c
Steven Penny <spen+3052@protonmail.com>
2023-10-29 19:08:40 -0500

    +   google_services_framework = 28

--- play/play.go
+++ play/play.go
@@ -10,142 +10,6 @@ import (
    "time"
 )
 
-func authorization(r *http.Request, a Access_Token) {
-   r.Header.Set("Authorization", "Bearer " + a.v.Get("Auth"))
-}
-
-func x_ps_rh(r *http.Request, c Checkin) error {
-   id, err := c.device_ID()
-   if err != nil {
-      return err
-   }
-   token, err := func() (string, error) {
-      var m protobuf.Message
-      m.Add(3, func(m *protobuf.Message) {
-         m.Add_String(1, strconv.FormatUint(id, 10))
-         m.Add(2, func(m *protobuf.Message) {
-            v := time.Now().UnixMicro()
-            m.Add_String(1, strconv.FormatInt(v, 10))
-         })
-      })
-      return compress(m)
-   }()
-   if err != nil {
-      return err
-   }
-   ps_rh, err := func() (string, error) {
-      var m protobuf.Message
-      m.Add(1, func(m *protobuf.Message) {
-         m.Add_String(1, token)
-      })
-      return compress(m)
-   }()
-   if err != nil {
-      return err
-   }
-   r.Header.Set("X-PS-RH", ps_rh)
-   return nil
-}
-
-func x_dfe_device_id(r *http.Request, c Checkin) error {
-   id, err := c.device_ID()
-   if err != nil {
-      return err
-   }
-   r.Header.Set("X-DFE-Device-ID", strconv.FormatUint(id, 16))
-   return nil
-}
-
-type Client struct {
-   Checkin Checkin
-   Token Access_Token
-}
-
-var Platforms = map[string]string{
-   // com.google.android.youtube
-   "0": "x86",
-   // com.miui.weather2
-   "1": "armeabi-v7a",
-   // com.kakaogames.twodin
-   "2": "arm64-v8a",
-}
-
-func compress(m protobuf.Message) (string, error) {
-   var b bytes.Buffer
-   w := gzip.NewWriter(&b)
-   _, err := w.Write(m.Append(nil))
-   if err != nil {
-      return "", err
-   }
-   if err := w.Close(); err != nil {
-      return "", err
-   }
-   return base64.URLEncoding.EncodeToString(b.Bytes()), nil
-}
-
-type Application struct {
-   ID string
-   Version uint64
-}
-
-func (a Application) APK(config string) string {
-   var b []byte
-   b = append(b, a.ID...)
-   b = append(b, '-')
-   if config != "" {
-      b = append(b, config...)
-      b = append(b, '-')
-   }
-   b = strconv.AppendUint(b, a.Version, 10)
-   b = append(b, ".apk"...)
-   return string(b)
-}
-
-func (a Application) OBB(role uint64) string {
-   var b []byte
-   if role >= 1 {
-      b = append(b, "patch"...)
-   } else {
-      b = append(b, "main"...)
-   }
-   b = append(b, '.')
-   b = strconv.AppendUint(b, a.Version, 10)
-   b = append(b, '.')
-   b = append(b, a.ID...)
-   b = append(b, ".obb"...)
-   return string(b)
-}
-
-type Device struct {
-   // developer.android.com/ndk/guides/abis
-   Platform string
-   // developer.android.com/guide/topics/manifest/supports-gl-texture-element
-   Texture []string
-   // developer.android.com/guide/topics/manifest/uses-library-element
-   Library []string
-   // developer.android.com/guide/topics/manifest/uses-feature-element
-   Feature []string
-}
-
-func user_agent(r *http.Request, single bool) {
-   var b []byte
-   // `sdk` is needed for `/fdfe/delivery`
-   b = append(b, "Android-Finsky (sdk="...)
-   // with `/fdfe/acquire`, requests will be rejected with certain apps, if the
-   // device was created with too low a version here:
-   b = strconv.AppendInt(b, google_services_framework, 10)
-   b = append(b, ",versionCode="...)
-   // for multiple APKs just tell the truth. for single APK we have to lie.
-   // below value is the last version that works.
-   if single {
-      b = strconv.AppendInt(b, 80919999, 10)
-   } else {
-      b = strconv.AppendInt(b, google_play_store, 10)
-   }
-   b = append(b, ')')
-   r.Header.Set("User-Agent", string(b))
-}
-
 const (
    google_play_store = 82941300
    // WARNING 28 is the last version that supports single APK
@@ -207,3 +71,138 @@ var Phone = Device{
    },
 }
 
+var Platforms = map[string]string{
+   // com.google.android.youtube
+   "0": "x86",
+   // com.miui.weather2
+   "1": "armeabi-v7a",
+   // com.kakaogames.twodin
+   "2": "arm64-v8a",
+}
+
+func authorization(r *http.Request, a Access_Token) {
+   r.Header.Set("Authorization", "Bearer " + a.v.Get("Auth"))
+}
+
+func compress(m protobuf.Message) (string, error) {
+   var b bytes.Buffer
+   w := gzip.NewWriter(&b)
+   _, err := w.Write(m.Append(nil))
+   if err != nil {
+      return "", err
+   }
+   if err := w.Close(); err != nil {
+      return "", err
+   }
+   return base64.URLEncoding.EncodeToString(b.Bytes()), nil
+}
+
+func user_agent(r *http.Request, single bool) {
+   var b []byte
+   // `sdk` is needed for `/fdfe/delivery`
+   b = append(b, "Android-Finsky (sdk="...)
+   // with `/fdfe/acquire`, requests will be rejected with certain apps, if the
+   // device was created with too low a version here:
+   b = strconv.AppendInt(b, google_services_framework, 10)
+   b = append(b, ",versionCode="...)
+   // for multiple APKs just tell the truth. for single APK we have to lie.
+   // below value is the last version that works.
+   if single {
+      b = strconv.AppendInt(b, 80919999, 10)
+   } else {
+      b = strconv.AppendInt(b, google_play_store, 10)
+   }
+   b = append(b, ')')
+   r.Header.Set("User-Agent", string(b))
+}
+
+func x_dfe_device_id(r *http.Request, c Checkin) error {
+   id, err := c.device_ID()
+   if err != nil {
+      return err
+   }
+   r.Header.Set("X-DFE-Device-ID", strconv.FormatUint(id, 16))
+   return nil
+}
+
+func x_ps_rh(r *http.Request, c Checkin) error {
+   id, err := c.device_ID()
+   if err != nil {
+      return err
+   }
+   token, err := func() (string, error) {
+      var m protobuf.Message
+      m.Add(3, func(m *protobuf.Message) {
+         m.Add_String(1, strconv.FormatUint(id, 10))
+         m.Add(2, func(m *protobuf.Message) {
+            v := time.Now().UnixMicro()
+            m.Add_String(1, strconv.FormatInt(v, 10))
+         })
+      })
+      return compress(m)
+   }()
+   if err != nil {
+      return err
+   }
+   ps_rh, err := func() (string, error) {
+      var m protobuf.Message
+      m.Add(1, func(m *protobuf.Message) {
+         m.Add_String(1, token)
+      })
+      return compress(m)
+   }()
+   if err != nil {
+      return err
+   }
+   r.Header.Set("X-PS-RH", ps_rh)
+   return nil
+}
+
+type Application struct {
+   ID string
+   Version uint64
+}
+
+func (a Application) APK(config string) string {
+   var b []byte
+   b = append(b, a.ID...)
+   b = append(b, '-')
+   if config != "" {
+      b = append(b, config...)
+      b = append(b, '-')
+   }
+   b = strconv.AppendUint(b, a.Version, 10)
+   b = append(b, ".apk"...)
+   return string(b)
+}
+
+func (a Application) OBB(role uint64) string {
+   var b []byte
+   if role >= 1 {
+      b = append(b, "patch"...)
+   } else {
+      b = append(b, "main"...)
+   }
+   b = append(b, '.')
+   b = strconv.AppendUint(b, a.Version, 10)
+   b = append(b, '.')
+   b = append(b, a.ID...)
+   b = append(b, ".obb"...)
+   return string(b)
+}
+
+type Client struct {
+   Checkin Checkin
+   Token Access_Token
+}
+
+type Device struct {
+   // developer.android.com/ndk/guides/abis
+   Platform string
+   // developer.android.com/guide/topics/manifest/supports-gl-texture-element
+   Texture []string
+   // developer.android.com/guide/topics/manifest/uses-library-element
+   Library []string
+   // developer.android.com/guide/topics/manifest/uses-feature-element
+   Feature []string
+}
