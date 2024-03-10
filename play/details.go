ba552a0490d7cf95c354a005a9a60eb4ed63e1fa
3052 <spen+3052@protonmail.com>
2024-03-10 10:21:19 -0500

    +   if v, ok := <-d.m.GetBytes(5); ok {

--- play/details.go
+++ play/details.go
@@ -9,106 +9,6 @@ import (
    "net/http"
 )
 
-func (d Details) String() string {
-   var b []byte
-   b = append(b, "downloads:"...)
-   if v, ok := d.Downloads(); ok {
-      b = fmt.Append(b, " ", encoding.Cardinal(v))
-   }
-   b = append(b, "\nfiles:"...)
-   for file := range d.File() {
-      if file >= 1 {
-         b = append(b, " OBB"...)
-      } else {
-         b = append(b, " APK"...)
-      }
-   }
-   b = append(b, "\nname:"...)
-   if v, ok := d.Name(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\noffered by:"...)
-   if v, ok := d.OfferedBy(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\nprice:"...)
-   if v, ok := d.Price(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   if v, ok := d.PriceCurrency(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\nrequires:"...)
-   if v, ok := d.Requires(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\nsize:"...)
-   if v, ok := d.Size(); ok {
-      b = fmt.Append(b, " ", encoding.Size(v))
-   }
-   b = append(b, "\nupdated on:"...)
-   if v, ok := d.UpdatedOn(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\nversion code:"...)
-   if v, ok := d.VersionCode(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\nversion name:"...)
-   if v, ok := d.VersionName(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   return string(b)
-}
-
-type Details struct {
-   Checkin Checkin
-   Token AccessToken
-   m protobuf.Message
-}
-
-func (d Details) Name() (string, bool) {
-   if v, ok := d.m.GetBytes(5); ok {
-      return string(v), true
-   }
-   return "", false
-}
-
-func (d Details) OfferedBy() (string, bool) {
-   if v, ok := d.m.GetBytes(6); ok {
-      return string(v), true
-   }
-   return "", false
-}
-
-// developer.android.com/guide/topics/manifest/manifest-element
-func (d Details) VersionCode() (uint64, bool) {
-   d.m, _ = d.m.Get(13)
-   d.m, _ = d.m.Get(1)
-   if v, ok := d.m.GetVarint(3); ok {
-      return uint64(v), true
-   }
-   return 0, false
-}
-
-// play.google.com/store/apps/details?id=com.google.android.youtube
-func (d Details) Downloads() (uint64, bool) {
-   d.m, _ = d.m.Get(13)
-   d.m, _ = d.m.Get(1)
-   if v, ok := d.m.GetVarint(70); ok {
-      return uint64(v), true
-   }
-   return 0, false
-}
-
-func (d Details) Price() (float64, bool) {
-   d.m, _ = d.m.Get(8)
-   if v, ok := d.m.GetVarint(1); ok {
-      return float64(v) / 1_000_000, true
-   }
-   return 0, false
-}
-
 func (d Details) Size() (uint64, bool) {
    d.m, _ = d.m.Get(13)
    d.m, _ = d.m.Get(1)
@@ -200,5 +100,105 @@ func (d Details) File() chan uint64 {
       close(vs)
    }()
    return vs
+}
+
+func (d Details) String() string {
+   var b []byte
+   b = append(b, "downloads:"...)
+   if v, ok := d.Downloads(); ok {
+      b = fmt.Append(b, " ", encoding.Cardinal(v))
+   }
+   b = append(b, "\nfiles:"...)
+   for file := range d.File() {
+      if file >= 1 {
+         b = append(b, " OBB"...)
+      } else {
+         b = append(b, " APK"...)
+      }
+   }
+   b = append(b, "\nname:"...)
+   if v, ok := d.Name(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\noffered by:"...)
+   if v, ok := d.OfferedBy(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\nprice:"...)
+   if v, ok := d.Price(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   if v, ok := d.PriceCurrency(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\nrequires:"...)
+   if v, ok := d.Requires(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\nsize:"...)
+   if v, ok := d.Size(); ok {
+      b = fmt.Append(b, " ", encoding.Size(v))
+   }
+   b = append(b, "\nupdated on:"...)
+   if v, ok := d.UpdatedOn(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\nversion code:"...)
+   if v, ok := d.VersionCode(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\nversion name:"...)
+   if v, ok := d.VersionName(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   return string(b)
+}
+
+type Details struct {
+   Checkin Checkin
+   Token AccessToken
+   m protobuf.Message
+}
+
+func (d Details) Name() (string, bool) {
+   if v, ok := <-d.m.GetBytes(5); ok {
+      return string(v), true
+   }
+   return "", false
+}
+
+func (d Details) OfferedBy() (string, bool) {
+   if v, ok := <-d.m.GetBytes(6); ok {
+      return string(v), true
+   }
+   return "", false
+}
+
+// developer.android.com/guide/topics/manifest/manifest-element
+func (d Details) VersionCode() (uint64, bool) {
+   d.m = <-d.m.Get(13)
+   d.m = <-d.m.Get(1)
+   if v, ok := <-d.m.GetVarint(3); ok {
+      return uint64(v), true
+   }
+   return 0, false
+}
+
+// play.google.com/store/apps/details?id=com.google.android.youtube
+func (d Details) Downloads() (uint64, bool) {
+   d.m = <-d.m.Get(13)
+   d.m = <-d.m.Get(1)
+   if v, ok := <-d.m.GetVarint(70); ok {
+      return uint64(v), true
+   }
+   return 0, false
+}
+
+func (d Details) Price() (float64, bool) {
+   d.m = <-d.m.Get(8)
+   if v, ok := <-d.m.GetVarint(1); ok {
+      return float64(v) / 1_000_000, true
+   }
+   return 0, false
 }
 
