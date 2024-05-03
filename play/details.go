9bbb5d3a18851442315d0dc7572112a2742c81cc
3052 <spen+3052@protonmail.com>
2024-05-02 21:33:47 -0500

    +var ABIs = []string{

--- play/details.go
+++ play/details.go
@@ -3,10 +3,118 @@ package play
 import (
    "154.pages.dev/encoding"
    "154.pages.dev/protobuf"
+   "errors"
    "fmt"
    "io"
    "net/http"
+   "strings"
 )
+
+func (g GoogleCheckin) Details(
+   auth GoogleAuth, doc string, single bool,
+) (*Details, error) {
+   req, err := http.NewRequest("GET", "https://android.clients.google.com", nil)
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Path = "/fdfe/details"
+   req.URL.RawQuery = "doc=" + doc
+   auth.authorization(req)
+   user_agent(req, single)
+   if err := g.x_dfe_device_id(req); err != nil {
+      return nil, err
+   }
+   res, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer res.Body.Close()
+   if res.StatusCode != http.StatusOK {
+      var b strings.Builder
+      res.Write(&b)
+      return nil, errors.New(b.String())
+   }
+   data, err := io.ReadAll(res.Body)
+   if err != nil {
+      return nil, err
+   }
+   var d Details
+   if err := d.m.Consume(data); err != nil {
+      return nil, err
+   }
+   d.m = <-d.m.Get(1)
+   d.m = <-d.m.Get(2)
+   d.m = <-d.m.Get(4)
+   return &d, nil
+}
+
+func (d Details) Name() (string, bool) {
+   if v, ok := <-d.m.GetBytes(5); ok {
+      return string(v), true
+   }
+   return "", false
+}
+
+func (d Details) Downloads() (uint64, bool) {
+   d.m = <-d.m.Get(13)
+   d.m = <-d.m.Get(1)
+   if v, ok := <-d.m.GetVarint(70); ok {
+      return uint64(v), true
+   }
+   return 0, false
+}
+
+func (d Details) String() string {
+   var b []byte
+   b = append(b, "details[6] ="...)
+   if v, ok := d.field_6(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\ndetails[8] ="...)
+   if v, ok := d.field_8_1(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   if v, ok := d.field_8_2(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\ndetails[13][1][4] ="...)
+   if v, ok := d.field_13_1_4(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\ndetails[13][1][16] ="...)
+   if v, ok := d.field_13_1_16(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\ndetails[13][1][17] ="...)
+   for file := range d.field_13_1_17() {
+      if file >= 1 {
+         b = append(b, " OBB"...)
+      } else {
+         b = append(b, " APK"...)
+      }
+   }
+   b = append(b, "\ndetails[13][1][82][1][1] ="...)
+   if v, ok := d.field_13_1_82_1_1(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\ndownloads ="...)
+   if v, ok := d.Downloads(); ok {
+      b = fmt.Append(b, " ", encoding.Cardinal(v))
+   }
+   b = append(b, "\nname ="...)
+   if v, ok := d.Name(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   b = append(b, "\nsize ="...)
+   if v, ok := d.size(); ok {
+      b = fmt.Append(b, " ", encoding.Size(v))
+   }
+   b = append(b, "\nversion code ="...)
+   if v, ok := d.version_code(); ok {
+      b = fmt.Append(b, " ", v)
+   }
+   return string(b)
+}
 
 type Details struct {
    m protobuf.Message
@@ -95,105 +203,4 @@ func (d Details) version_code() (uint64, bool) {
       return uint64(v), true
    }
    return 0, false
-}
-
-func (g GoogleCheckin) Details(
-   auth GoogleAuth, doc string, single bool,
-) (*Details, error) {
-   req, err := http.NewRequest("GET", "https://android.clients.google.com", nil)
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = "/fdfe/details"
-   req.URL.RawQuery = "doc=" + doc
-   auth.authorization(req)
-   user_agent(req, single)
-   if err := g.x_dfe_device_id(req); err != nil {
-      return nil, err
-   }
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   data, err := io.ReadAll(res.Body)
-   if err != nil {
-      return nil, err
-   }
-   var d Details
-   if err := d.m.Consume(data); err != nil {
-      return nil, err
-   }
-   d.m = <-d.m.Get(1)
-   d.m = <-d.m.Get(2)
-   d.m = <-d.m.Get(4)
-   return &d, nil
-}
-
-func (d Details) Name() (string, bool) {
-   if v, ok := <-d.m.GetBytes(5); ok {
-      return string(v), true
-   }
-   return "", false
-}
-
-func (d Details) Downloads() (uint64, bool) {
-   d.m = <-d.m.Get(13)
-   d.m = <-d.m.Get(1)
-   if v, ok := <-d.m.GetVarint(70); ok {
-      return uint64(v), true
-   }
-   return 0, false
-}
-
-func (d Details) String() string {
-   var b []byte
-   b = append(b, "details[6] ="...)
-   if v, ok := d.field_6(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\ndetails[8] ="...)
-   if v, ok := d.field_8_1(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   if v, ok := d.field_8_2(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\ndetails[13][1][4] ="...)
-   if v, ok := d.field_13_1_4(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\ndetails[13][1][16] ="...)
-   if v, ok := d.field_13_1_16(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\ndetails[13][1][17] ="...)
-   for file := range d.field_13_1_17() {
-      if file >= 1 {
-         b = append(b, " OBB"...)
-      } else {
-         b = append(b, " APK"...)
-      }
-   }
-   b = append(b, "\ndetails[13][1][82][1][1] ="...)
-   if v, ok := d.field_13_1_82_1_1(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\ndownloads ="...)
-   if v, ok := d.Downloads(); ok {
-      b = fmt.Append(b, " ", encoding.Cardinal(v))
-   }
-   b = append(b, "\nname ="...)
-   if v, ok := d.Name(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   b = append(b, "\nsize ="...)
-   if v, ok := d.size(); ok {
-      b = fmt.Append(b, " ", encoding.Size(v))
-   }
-   b = append(b, "\nversion code ="...)
-   if v, ok := d.version_code(); ok {
-      b = fmt.Append(b, " ", v)
-   }
-   return string(b)
 }
--- play/play.go
+++ play/play.go
@@ -10,55 +10,6 @@ import (
    "time"
 )
 
-var Phone = GoogleDevice{
-   Feature: []string{
-      // app.source.getcontact
-      "android.hardware.location.gps",
-      // br.com.rodrigokolb.realdrum
-      "android.software.midi",
-      // com.app.xt
-      "android.hardware.camera.front",
-      // com.cabify.rider
-      "android.hardware.camera.flash",
-      // com.clearchannel.iheartradio.controller
-      "android.hardware.microphone",
-      // com.google.android.apps.walletnfcrel
-      "android.software.device_admin",
-      // com.google.android.youtube
-      "android.hardware.touchscreen",
-      "android.hardware.wifi",
-      // com.madhead.tos.zh
-      "android.hardware.sensor.accelerometer",
-      // com.pinterest
-      "android.hardware.camera",
-      "android.hardware.location",
-      "android.hardware.screen.portrait",
-      // com.sygic.aura
-      "android.hardware.location.network",
-      // com.xiaomi.smarthome
-      "android.hardware.bluetooth",
-      "android.hardware.bluetooth_le",
-      "android.hardware.camera.autofocus",
-      "android.hardware.usb.host",
-      // kr.sira.metal
-      "android.hardware.sensor.compass",
-      // org.thoughtcrime.securesms
-      "android.hardware.telephony",
-   },
-   Library: []string{
-      // com.amctve.amcfullepisodes
-      "org.apache.http.legacy",
-      // com.binance.dev
-      "android.test.runner",
-   },
-   Texture: []string{
-      // com.instagram.android
-      "GL_OES_compressed_ETC1_RGB8_texture",
-      // com.kakaogames.twodin
-      "GL_KHR_texture_compression_astc_ldr",
-   },
-}
-
 const android_api = 31
 
 // play.google.com/store/apps/details?id=com.google.android.apps.youtube.unplugged
@@ -87,6 +38,7 @@ func (s StoreApp) OBB(v uint64) string {
    b = fmt.Append(b, ".", s.Version, ".", s.ID, ".obb")
    return string(b)
 }
+
 func (g GoogleCheckin) x_ps_rh(req *http.Request) error {
    id, err := g.device_id()
    if err != nil {
@@ -131,15 +83,6 @@ const gl_es_version = 0x30001
 
 const google_play_store = 82941300
 
-var ABIs = map[int]string{
-   // com.google.android.youtube
-   0: "x86",
-   // com.sygic.aura
-   1: "armeabi-v7a",
-   // com.kakaogames.twodin
-   2: "arm64-v8a",
-}
-
 func user_agent(req *http.Request, single bool) {
    var b []byte
    // `sdk` is needed for `/fdfe/delivery`
@@ -157,21 +100,6 @@ func user_agent(req *http.Request, single bool) {
    }
    b = append(b, ')')
    req.Header.Set("User-Agent", string(b))
-}
-
-// developer.android.com/ndk/guides/abis
-type ABI int
-
-func (a *ABI) Set(s string) error {
-   _, err := fmt.Sscan(s, a)
-   if err != nil {
-      return err
-   }
-   return nil
-}
-
-func (a ABI) String() string {
-   return ABIs[int(a)]
 }
 
 func (g GoogleAuth) authorization(req *http.Request) {
@@ -192,4 +120,66 @@ type GoogleDevice struct {
    Feature []string
    Library []string
    Texture []string
+}
+
+// developer.android.com/ndk/guides/abis
+var ABIs = []string{
+   // com.google.android.youtube
+   "x86",
+   // com.sygic.aura
+   "armeabi-v7a",
+   // com.kakaogames.twodin
+   "arm64-v8a",
+}
+
+var devices = map[string]GoogleDevice{
+   "TV": {},
+   "Phone": {
+      Feature: []string{
+         // app.source.getcontact
+         "android.hardware.location.gps",
+         // br.com.rodrigokolb.realdrum
+         "android.software.midi",
+         // com.app.xt
+         "android.hardware.camera.front",
+         // com.cabify.rider
+         "android.hardware.camera.flash",
+         // com.clearchannel.iheartradio.controller
+         "android.hardware.microphone",
+         // com.google.android.apps.walletnfcrel
+         "android.software.device_admin",
+         // com.google.android.youtube
+         "android.hardware.touchscreen",
+         "android.hardware.wifi",
+         // com.madhead.tos.zh
+         "android.hardware.sensor.accelerometer",
+         // com.pinterest
+         "android.hardware.camera",
+         "android.hardware.location",
+         "android.hardware.screen.portrait",
+         // com.sygic.aura
+         "android.hardware.location.network",
+         // com.xiaomi.smarthome
+         "android.hardware.bluetooth",
+         "android.hardware.bluetooth_le",
+         "android.hardware.camera.autofocus",
+         "android.hardware.usb.host",
+         // kr.sira.metal
+         "android.hardware.sensor.compass",
+         // org.thoughtcrime.securesms
+         "android.hardware.telephony",
+      },
+      Library: []string{
+         // com.amctve.amcfullepisodes
+         "org.apache.http.legacy",
+         // com.binance.dev
+         "android.test.runner",
+      },
+      Texture: []string{
+         // com.instagram.android
+         "GL_OES_compressed_ETC1_RGB8_texture",
+         // com.kakaogames.twodin
+         "GL_KHR_texture_compression_astc_ldr",
+      },
+   },
 }
