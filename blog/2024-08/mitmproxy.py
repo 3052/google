9027e5b6c3259fd067f814d3a1957915b6287867
3052 <spen+3052@protonmail.com>
2024-08-28 05:22:56 -0500

    +   if f.request.path.startswith('/v1/getPoIntegrityToken'):

--- blog/2024-08/mitmproxy.py
+++ blog/2024-08/mitmproxy.py
@@ -4,7 +4,5 @@ def running():
    ctx.options.showhost = True
 
 def request(f: http.HTTPFlow) -> None:
-   if f.request.path.startswith('/play-apps-download-default/download/by-id/'):
-      f.kill()
-   if f.request.path.startswith('/fdfe/replicateLibrary'):
+   if f.request.path.startswith('/v1/getPoIntegrityToken'):
       f.kill()
