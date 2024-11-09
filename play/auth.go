447c16dc4f6857bc5f1f776686c200515c9199db
3052 <spen+3052@protonmail.com>
2024-11-09 07:57:43 -0600

    +type GoogleToken struct {

--- play/auth.go
+++ play/auth.go
@@ -20,6 +20,35 @@ func (g *GoogleToken) token() string {
    return g.Values["Token"]
 }
 
+type GoogleToken struct {
+   Values Values
+}
+
+func (GoogleToken) Marshal(token string) ([]byte, error) {
+   resp, err := http.PostForm(
+      "https://android.googleapis.com/auth", url.Values{
+         "ACCESS_TOKEN": {"1"},
+         "Token":        {token},
+         "service":      {"ac2dm"},
+      },
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      var b strings.Builder
+      resp.Write(&b)
+      return nil, errors.New(b.String())
+   }
+   return io.ReadAll(resp.Body)
+}
+
+func (g *GoogleToken) Unmarshal(data []byte) error {
+   g.Values = Values{}
+   return g.Values.Set(string(data))
+}
+
 func (g *GoogleToken) Auth() (*GoogleAuth, error) {
    resp, err := http.PostForm(
       "https://android.googleapis.com/auth", url.Values{
@@ -58,32 +87,3 @@ func (v Values) Set(query string) error {
 }
 
 type Values map[string]string
-
-type GoogleToken struct {
-   Values Values
-}
-
-func (GoogleToken) Marshal(token string) ([]byte, error) {
-   resp, err := http.PostForm(
-      "https://android.googleapis.com/auth", url.Values{
-         "ACCESS_TOKEN": {"1"},
-         "Token":        {token},
-         "service":      {"ac2dm"},
-      },
-   )
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      var b strings.Builder
-      resp.Write(&b)
-      return nil, errors.New(b.String())
-   }
-   return io.ReadAll(resp.Body)
-}
-
-func (g *GoogleToken) Unmarshal(data []byte) error {
-   g.Values = Values{}
-   return g.Values.Set(string(data))
-}
